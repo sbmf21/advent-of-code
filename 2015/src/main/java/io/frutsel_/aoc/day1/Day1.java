24268813b20a278f1d5a60d416e4d8312950e89c
Boukje Maria Steur <boukje@switchware.io>
2020-12-02 20:52:30 +0100

    2015 day one

---
+++ 2015/src/main/java/io/frutsel_/aoc/day1/Day1.java
@@ -0,0 +1,34 @@
+package io.frutsel_.aoc.day1;
+
+import io.frutsel_.aoc.APart;
+import io.frutsel_.aoc.AdventOfCode;
+import io.frutsel_.aoc.IDay;
+
+import java.io.IOException;
+
+public class Day1 implements IDay {
+
+    public char[] loadInstructions() {
+        try (var reader = AdventOfCode.getResource("floors")) {
+            StringBuilder input = new StringBuilder();
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                input.append(line);
+            }
+
+            return input.toString().toCharArray();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new char[0];
+        }
+    }
+
+    @Override public APart<?>[] parts() {
+        return new APart[]{new Part1(this), new Part2(this)};
+    }
+
+    @Override public int dayNumber() {
+        return 1;
+    }
+}
