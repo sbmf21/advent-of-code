bc9fc4367f172ec1a4a3a582981064f5b86d05af
Boukje Maria Steur <boukje@switchware.io>
2020-12-04 06:55:31 +0100

    2020 day 4

---
+++ 2020/Day 4/functions.lua
@@ -0,0 +1,60 @@
+function split(input, match)
+    local sparts = {}
+
+    for token in input:gmatch('[^' .. match .. ']+') do
+        sparts[#sparts + 1] = token
+    end
+
+    return sparts
+end
+
+local function addCurrent(passports, current)
+    passports[#passports + 1] = current
+
+    return {}
+end
+
+local function parseLine(current, line)
+    local fields = split(line, ' ')
+ 
+    for _, field in ipairs(fields) do
+        local parts = split(field, ':')
+       
+        current[parts[1]] = parts[2]
+    end
+end
+
+ function parsePassports()
+    local lines = io.lines('passports.txt')
+    local passports = {}; local current = {}
+
+    for line in lines do
+        if line == '' then
+            current = addCurrent(passports, current)
+        end
+
+        parseLine(current, line)
+    end
+
+    addCurrent(passports, current)
+
+    return passports
+end
+
+function countValidPassports(isValid)
+    local count = 0
+
+    for _, passport in ipairs(parsePassports()) do
+        if isValid(passport) then
+            count = count + 1
+        end
+    end
+
+    return count
+end
+
+function clamp(value, min, max)
+    value = tonumber(value)
+
+    return value >= min and value <= max
+end
--- 2020/Day 4/part1.lua
+++ 2020/Day 4/part1.lua
@@ -1,1 +1,14 @@
 require 'functions'
+
+local function isValid(passport)
+    for _, key in ipairs({'byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'}) do
+        if passport[key] ~= null then
+        else
+            return false
+        end
+    end
+
+    return true
+end
+
+print(countValidPassports(isValid))
