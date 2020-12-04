979c1afe281a6b4ec6c82b5bb07ea20ddcd13b88
Boukje Maria Steur <boukje@switchware.io>
2020-12-04 07:19:31 +0100

    small refactor

--- 2020/Day 4/part2.lua
+++ 2020/Day 4/part2.lua
@@ -1,22 +1,14 @@
 require 'functions'
 
 local keys = {
-    byr = function (value)
-        return clamp(value, 1920, 2002)
-    end,
-    iyr = function(value)
-        return clamp(value, 2010, 2020)
-    end,
-    eyr = function(value)
-        return clamp(value, 2020, 2030)
-    end,
+    byr = function (value) return between(value, 1920, 2002) end,
+    iyr = function(value) return between(value, 2010, 2020) end,
+    eyr = function(value) return between(value, 2020, 2030) end,
     hgt = function(value)
         if value:find('cm') then
-            value = value:sub(0, value:len() - 2)
-            return clamp(value, 150, 193)
+            return subBetween(value, 150, 193)
         elseif value:find('in') then
-            value = value:sub(0, value:len() - 2)
-            return clamp(value, 59, 76)
+            return subBetween(value, 59, 76)
         end
         
         return false
@@ -24,8 +16,7 @@ local keys = {
     hcl = function(value)
         if value:sub(0, 1):match('#') and value:len() == 7 then
             for i = 2,7 do
-                if value:sub(i, i):match('[abcdef%d]') then
-                else
+                if not value:sub(i, i):match('[abcdef%d]') then
                     return false
                 end
             end
@@ -46,7 +37,7 @@ local keys = {
     end,
     pid = function(value)
         if value:len() == 9 then
-            return clamp(value, 0, 999999999)
+            return between(value, 0, 999999999)
         end
 
         return false
