aa9edba07d3168accaea00e7128e7d094442e3f9
Boukje Maria Steur <boukje@switchware.io>
2020-12-05 11:14:34 +0100

    2020:5 180 > 40

--- 2020/Day 5/functions.lua
+++ 2020/Day 5/functions.lua
@@ -1,141 +1,21 @@
-local cache = {
-    rows = {},
-    columns = {}
-}
-
-local function loadBoardingPasses() return io.lines('boarding-passes.txt') end
-local function extractRow(pass) return pass:sub(1, 7) end
-local function extractColumn(pass) return pass:sub(8, 10) end
-
-local function isValidRow(pass)
-    local row = extractRow(pass)
-
-    for i = 1, #row do
-        if not row:sub(i, i):match('[FB]') then
-            return false
-        end
-    end
-
-    return true
-end
-
-local function isValidColumn(pass)
-    local column = extractColumn(pass)
-
-    for i = 1, #column do
-        if not column:sub(i, i):match('[RL]') then
-            return false
-        end
-    end
-
-    return true
-end
-
-local function isValidPass(pass)
-    if #pass ~= 10 then
-        return false
-    end
-
-    return isValidRow(pass)
-       and isValidColumn(pass)
-end
-
-local function calculateDifference(min, max)
-    return math.floor(math.abs(max - min) / 2 + 0.5)
-end
-
-local function convertRow(line)
-    local row = extractRow(line)
-
-    if(cache.rows[row] ~= nil) then
-        return cache.rows[row]
-    end
-
-    local min = 0; local max = 127
-
-    for i = 1, #row do
-        local c = row:sub(i, i)
-
-        if c == 'F' then
-            max = max - calculateDifference(min, max)
-        else
-            min = min + calculateDifference(min, max)
-        end
-    end
-    
-    if min ~= max then
-        error(min .. ' and ' .. max .. ' did not match for pass ' .. row)
-    end
-
-    cache.rows[row] = min
-
-    return min
-end
-
-local function convertColumn(line)
-    local column = extractColumn(line)
-
-    if(cache.columns[column] ~= nil) then
-        return cache.columns[column]
-    end
-
-    local min = 0; local max = 7
-
-    for i = 1, #column do
-        local c = column:sub(i, i)
-
-        if c == 'L' then
-            max = max - calculateDifference(min, max)
-        else
-            min = min + calculateDifference(min, max)
-        end
-    end
-
-    cache.columns[column] = min
-
-    return min
-end
-
-function calculateId(row, column)
-    return row * 8 + column
-end
-
-local function convertPass(line)
-    local pass = {
-        row = convertRow(line),
-        column = convertColumn(line)
-    }
-
-    pass.id = calculateId(pass.row, pass.column)
-
-    return pass
-end
-
 function parseBoardingPasses()
-    local input = loadBoardingPasses()
-    local passes = {}
+    local ids = {}
 
-    for pass in input do
-        if isValidPass(pass) then
-            passes[#passes + 1] = convertPass(pass)
-        else
-            print('Invalid pass: ' .. pass)
-        end
+    for line in io.lines('boarding-passes.txt') do
+        ids[#ids + 1] = tonumber(line:gsub('F', '0'):gsub('L', '0'):gsub('B', '1'):gsub('R', 1), 2)
     end
 
-    return passes
+    table.sort(ids)
+
+    return ids
 end
 
-function parseBoardingPassesToIndices()
-    local indices = {}
-
-    for _, pass in ipairs(parseBoardingPasses()) do
-        if indices[pass.row] == nil then
-            indices[pass.row] = {}
+function has(ids, id)
+    for _, value in ipairs(ids) do
+        if value == id then
+            return true
         end
-
-        indices[pass.row][pass.column] = pass
     end
 
-    return indices
+    return false
 end
--- 2020/Day 5/part2.lua
+++ 2020/Day 5/part2.lua
@@ -1,19 +1,15 @@
 require 'functions'
 
-local function findId()
-    local index = parseBoardingPassesToIndices()
+local function find()
+    local ids = parseBoardingPasses()
 
-    -- rows start at 0, end at 127; not first; not last
-    for row = 1, 126 do
-        -- columns start at 0, end at 7, id - 1 and id + 1 must exist so 0 and 7 cant be
-        for column = 1, 6 do
-            if index[row] ~= nil and index[row][column - 1] ~= nil and index[row][column] == nil and index[row][column + 1] ~= nil then
-                return calculateId(row, column)
-            end
+    for id = ids[1], ids[#ids] do
+        if not has(ids, id) and has(ids, id - 1) and has(ids, id + 1) then
+            return id
         end
     end
-    
+
     error('404')
 end
 
-print(findId())
+print(find())
