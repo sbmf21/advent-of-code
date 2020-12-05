040ddee4067ebb744761288ac09125a5c5ed160c
Boukje Maria Steur <boukje@switchware.io>
2020-12-05 19:42:24 +0100

    Ready for day 6

---
+++ 2020/kotlin/src/main/kotlin/io/frutsel_/aoc/days/Day6.kt
@@ -0,0 +1,13 @@
+package io.frutsel_.aoc.days
+
+import io.frutsel_.aoc.ADay
+import io.frutsel_.aoc.Aoc
+
+@Suppress("unused")
+class Day6(aoc: Aoc) : ADay(aoc) {
+    override fun number(): Int = 6
+
+    override fun part1(): Number = 0
+
+    override fun part2(): Number = 0
+}
