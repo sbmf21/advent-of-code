fdfffeb83175adf2216a798191af80e2781061cd
Boukje Maria Steur <boukje@switchware.io>
2020-12-05 23:01:14 +0100

    fix test

--- 2020/lua/Day 5/test.lua
+++ 2020/lua/Day 5/test.lua
@@ -3,7 +3,7 @@
 require 'functions'
 
 local part1 = part1(); local expect1 = 922
-local part2 = part2(); local expect2 = 0
+local part2 = part2(); local expect2 = 747
 
 if part1 ~= expect1 then error('Part 1: '..part1..' did not match expected '..expect1) end
 if part2 ~= expect2 then error('Part 2: '..part2..' did not match expected '..expect2) end
