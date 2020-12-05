095cb82b48ebbca98de598c44e3b78d258c2611a
Boukje Maria Steur <boukje@switchware.io>
2020-12-05 11:14:34 +0100

    2020:5 180 > 40

--- 2020/Day 5/part1.lua
+++ 2020/Day 5/part1.lua
@@ -1,18 +1,4 @@
 require 'functions'
 
-local function findHighestId()
-    local passes = parseBoardingPasses()
-    local id = passes[1].id
-    
-    for i = 2, #passes do
-        local pid = passes[i].id
-    
-        if pid > id then
-            id = pid
-        end
-    end
-
-    return id
-end
-
-print(findHighestId())
+local ids = parseBoardingPasses()
+print(ids[#ids])
