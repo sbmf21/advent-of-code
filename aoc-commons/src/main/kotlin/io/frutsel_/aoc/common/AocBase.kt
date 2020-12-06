d4a4049fb0b5cfdde5d66d65c23a2811452c70b2
Boukje Maria Steur <boukje@switchware.io>
2020-12-06 23:52:48 +0100

    Add command line day filter to commons

--- aoc-commons/src/main/kotlin/io/frutsel_/aoc/common/AocBase.kt
+++ aoc-commons/src/main/kotlin/io/frutsel_/aoc/common/AocBase.kt
@@ -1,13 +1,34 @@
 package io.frutsel_.aoc.common
 
 import org.reflections.Reflections
+import java.util.regex.Pattern
 
-abstract class AocBase {
+abstract class AocBase(val name: String) {
 
-    fun findDays() = Reflections("${this::class.java.packageName}.days")
+    private var pattern = Pattern.compile("--day(?:=(?<day>\\d+))?")
+    private var runDay: Int? = null
+
+    fun init(args: Array<String>) = args.forEach {
+        val matcher = pattern.matcher(it)
+
+        if (matcher.matches()) {
+            val day = matcher.group("day")
+
+            if (day == null) {
+                print("Enter day to run: ")
+
+                this.runDay = readLine()?.toInt()
+            } else {
+                this.runDay = day.toInt()
+            }
+        }
+    }
+
+    fun findDays(): List<ADay> = Reflections("${this::class.java.packageName}.days")
         .getSubTypesOf(ADay::class.java)
         .map { it.getDeclaredConstructor(this::class.java).newInstance(this) }
         .sortedBy { it.number() }
+        .filter { if (runDay == null) true else it.number() == runDay }
 
     fun file(day: ADay): List<String> = day.javaClass
         .getResourceAsStream("/input/day${day.number()}.txt")
