817a9711c1ced63836894822d52fea45beef5208
Boukje Maria Steur <boukje@switchware.io>
2020-12-07 00:03:08 +0100

    Use reporting in 2015 and 2020

--- 2015/src/main/java/io/frutsel_/aoc/Aoc.java
+++ 2015/src/main/java/io/frutsel_/aoc/Aoc.java
@@ -1,35 +1,18 @@
 package io.frutsel_.aoc;
 
-import io.frutsel_.aoc.common.ADay;
 import io.frutsel_.aoc.common.AocBase;
 
-public class Aoc extends AocBase implements Runnable {
-
-    public static void main(String[] args) {
-        var aoc = new Aoc();
-
-        aoc.init(args);
-        aoc.run();
-    }
+public class Aoc extends AocBase {
 
     public Aoc() {
         super("2015");
     }
 
-    @Override
-    public void run() {
-        this.findDays().forEach(this::runDay);
+    public static void main(String[] args) {
+        var aoc = new Aoc();
 
-        ReportGenerator.print();
-    }
-
-    private void runDay(ADay day) {
-        var start = System.currentTimeMillis();
-        var part1 = day.part1();
-        var m = System.currentTimeMillis();
-        var part2 = day.part2();
-        var end = System.currentTimeMillis();
-
-        ReportGenerator.add(day, start, m, end, part1, part2);
+        aoc.init(args);
+        aoc.runDays();
+        aoc.report();
     }
 }
