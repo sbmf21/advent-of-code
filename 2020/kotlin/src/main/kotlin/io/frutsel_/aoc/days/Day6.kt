a67df3dc4fef12b14419ada8582db34d4e49a320
Boukje Maria Steur <boukje@switchware.io>
2020-12-06 10:07:45 +0100

    little refac

--- 2020/kotlin/src/main/kotlin/io/frutsel_/aoc/days/Day6.kt
+++ 2020/kotlin/src/main/kotlin/io/frutsel_/aoc/days/Day6.kt
@@ -9,13 +9,11 @@ class Day6(aoc: Aoc) : ADay(aoc) {
 
     override fun number(): Int = 6
 
-    override fun part1(): Number = lines
-        .map { it.split("\n").joinToString(separator = "") }
+    override fun part1(): Number = lines.map { it.split("\n").joinToString(separator = "") }
         .map { it.toCharArray().distinct().size }
         .sum()
 
-    override fun part2(): Number = lines
-        .map { it.split("\n") }
-        .map { it.map { it.toCharArray().distinct() }.reduce { acc, next -> acc.intersect(next).toList() }.size }
+    override fun part2(): Number = lines.map { it.split("\n") }
+        .map { it.map { ln -> ln.toCharArray().distinct() }.reduce { acc, next -> acc.intersect(next).toList() }.size }
         .sum()
 }
