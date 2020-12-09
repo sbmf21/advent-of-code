8094734054bdb9c91f831ff1ffc07fc3416aa868
Boukje Maria Steur <boukje@switchware.io>
2020-12-09 23:09:40 +0100

    #1 attempt 6

--- ci/aoc2020.sh
+++ ci/aoc2020.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-if [ -d /aoc-commons/.m2/repository/io/frutsel_/adventofcode.common ]; then
-    if [ -d /2020/kotlin/.m2/repository/io/frutsel_/adventofcode.common ]; then
-        rm -rf /2020/kotlin/.m2/repository/io/frutsel_/adventofcode.common
+if [ -d aoc-commons/.m2/repository/io/frutsel_/adventofcode.common ]; then
+    if [ -d 2020/kotlin/.m2/repository/io/frutsel_/adventofcode.common ]; then
+        rm -rf 2020/kotlin/.m2/repository/io/frutsel_/adventofcode.common
     fi
 
     mv aoc-commons/.m2/repository/io/frutsel_/adventofcode.common 2020/kotlin/.m2/repository/io/frutsel_/
