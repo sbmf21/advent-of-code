8d92f5d4aa46c2c69aedbd298efbf01f49b86a04
Boukje Maria Steur <boukje@switchware.io>
2020-12-09 23:09:40 +0100

    #1 attempt 6

--- ci/aoc2015.sh
+++ ci/aoc2015.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-if [ -d /aoc-commons/.m2/repository/io/frutsel_/adventofcode.common ]; then
-    if [ -d /2015/.m2/repository/io/frutsel_/adventofcode.common ]; then
-        rm -rf /2015/.m2/repository/io/frutsel_/adventofcode.common
+if [ -d aoc-commons/.m2/repository/io/frutsel_/adventofcode.common ]; then
+    if [ -d 2015/.m2/repository/io/frutsel_/adventofcode.common ]; then
+        rm -rf 2015/.m2/repository/io/frutsel_/adventofcode.common
     fi
 
     mv aoc-commons/.m2/repository/io/frutsel_/adventofcode.common 2015/.m2/repository/io/frutsel_/
