b6b9fcd4deafd3f383be63c8d66d60120fb1a665
Boukje Maria Steur <boukje@switchware.io>
2020-12-09 22:07:08 +0100

    Undo this

--- 2020/kotlin/src/main/kotlin/io/frutsel_/aoc/days/Day9.kt
+++ 2020/kotlin/src/main/kotlin/io/frutsel_/aoc/days/Day9.kt
@@ -6,21 +6,20 @@ import io.frutsel_.aoc.common.ADay
 class Day9(aoc: Aoc) : ADay(aoc) {
 
     private val numbers = input.map { it.toLong() }
-    private var weak = -1
 
     override fun number(): Int = 9
 
     override fun part1(): Int {
         for (i in 25 until numbers.size) {
             if (!isSum(i)) {
-                weak = numbers[i].toInt()
-                return weak
+                return numbers[i].toInt()
             }
         }
         return -1
     }
 
     override fun part2(): Int {
+        val weak = part1().toLong()
         for (i in numbers.indices) {
             var acc = numbers[i]
 
@@ -31,7 +30,7 @@ class Day9(aoc: Aoc) : ADay(aoc) {
                     break
                 }
 
-                if (acc == weak.toLong()) {
+                if (acc == weak) {
                     val subList = numbers.subList(i, j).map { it.toInt() }
                     return subList.minOrNull()?.plus(subList.maxOrNull()!!)!!
                 }
