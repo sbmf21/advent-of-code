2bcebce1c0de95541462cdf7288e566728d985df
Boukje <boukje@switchware.io>
2021-12-01 07:14:01 +0100

    prpare 2021 day 2

---
+++ 2021/kotlin/src/main/kotlin/nl/sbmf21/aoc21/days/Day2.kt
@@ -0,0 +1,13 @@
+package nl.sbmf21.aoc21.days
+
+import nl.sbmf21.aoc.common.ADay
+import nl.sbmf21.aoc21.Aoc
+
+class Day2(aoc: Aoc) : ADay(aoc) {
+
+    override fun number() = 2
+
+    override fun part1() = -1
+
+    override fun part2() = -1
+}
