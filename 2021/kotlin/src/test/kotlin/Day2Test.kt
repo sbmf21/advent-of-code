f190f5b2d93a890b86eac1a0ca08184c4f7d66c5
Boukje <boukje@switchware.io>
2021-12-01 07:14:01 +0100

    prpare 2021 day 2

---
+++ 2021/kotlin/src/test/kotlin/Day2Test.kt
@@ -0,0 +1,15 @@
+import nl.sbmf21.aoc21.Aoc
+import nl.sbmf21.aoc21.days.Day2
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class Day2Test {
+
+    private var day = Day2(Aoc())
+
+    @Test
+    fun testPart1Output() = assertEquals(-1, day.part1())
+
+    @Test
+    fun testPart2Output() = assertEquals(-1, day.part2())
+}
