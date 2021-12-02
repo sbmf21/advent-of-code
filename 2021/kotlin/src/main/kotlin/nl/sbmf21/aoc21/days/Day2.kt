fb7a38eee2794ee9922d9c4e09bf005ff7cbceaa
Boukje <boukje@switchware.io>
2021-12-02 06:28:08 +0100

    []=[y=2021,d=2]

--- 2021/kotlin/src/main/kotlin/nl/sbmf21/aoc21/days/Day2.kt
+++ 2021/kotlin/src/main/kotlin/nl/sbmf21/aoc21/days/Day2.kt
@@ -5,7 +5,44 @@ import nl.sbmf21.aoc21.Aoc
 
 class Day2(aoc: Aoc, number: Int) : ADay(aoc, number) {
 
-    override fun part1() = -1
+    private val actions = input
+        .map { it.split(' ') }
+        .map { Pair(it[0], it[1].toInt()) }
 
-    override fun part2() = -1
+    override fun part1(): Int {
+
+        var x = 0
+        var y = 0
+
+        run(
+            forward = { x += it },
+            up = { y -= it },
+            down = { y += it }
+        )
+
+        return x * y
+    }
+
+    override fun part2(): Int {
+
+        var x = 0
+        var y = 0
+        var p = 0
+
+        run(
+            forward = { x += it; y += (p * it) },
+            up = { p -= it },
+            down = { p += it }
+        )
+
+        return x * y
+    }
+
+    private fun run(forward: (Int) -> Unit, up: (Int) -> Unit, down: (Int) -> Unit) = actions.forEach {
+        when (it.first) {
+            "forward" -> forward(it.second)
+            "up" -> up(it.second)
+            "down" -> down(it.second)
+        }
+    }
 }
