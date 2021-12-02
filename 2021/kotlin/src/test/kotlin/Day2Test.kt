fa87671ad3441d8e6c5f4fc8cbd797d34cc9e71e
Boukje <boukje@switchware.io>
2021-12-02 06:28:08 +0100

    []=[y=2021,d=2]

--- 2021/kotlin/src/test/kotlin/Day2Test.kt
+++ 2021/kotlin/src/test/kotlin/Day2Test.kt
@@ -8,8 +8,8 @@ class Day2Test {
     private var day = Day2(Aoc(), 2)
 
     @Test
-    fun testPart1Output() = assertEquals(-1, day.part1())
+    fun testPart1Output() = assertEquals(1690020, day.part1())
 
     @Test
-    fun testPart2Output() = assertEquals(-1, day.part2())
+    fun testPart2Output() = assertEquals(1408487760, day.part2())
 }
