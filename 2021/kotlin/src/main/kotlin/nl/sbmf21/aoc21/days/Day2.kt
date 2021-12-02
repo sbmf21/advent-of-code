a7440df8080500d6f5372ab9a64c08b7429a701a
Boukje <boukje@switchware.io>
2021-12-02 06:40:22 +0100

    it.shorten()

--- 2021/kotlin/src/main/kotlin/nl/sbmf21/aoc21/days/Day2.kt
+++ 2021/kotlin/src/main/kotlin/nl/sbmf21/aoc21/days/Day2.kt
@@ -3,6 +3,8 @@ package nl.sbmf21.aoc21.days
 import nl.sbmf21.aoc.common.ADay
 import nl.sbmf21.aoc21.Aoc
 
+typealias Run = (Int) -> Unit
+
 class Day2(aoc: Aoc, number: Int) : ADay(aoc, number) {
 
     private val actions = input
@@ -10,35 +12,16 @@ class Day2(aoc: Aoc, number: Int) : ADay(aoc, number) {
         .map { Pair(it[0], it[1].toInt()) }
 
     override fun part1(): Int {
-
-        var x = 0
-        var y = 0
-
-        run(
-            forward = { x += it },
-            up = { y -= it },
-            down = { y += it }
-        )
-
-        return x * y
+        var x = 0; var y = 0
+        run({ x += it }, { y -= it }, { y += it }).also { return x * y }
     }
 
     override fun part2(): Int {
-
-        var x = 0
-        var y = 0
-        var p = 0
-
-        run(
-            forward = { x += it; y += (p * it) },
-            up = { p -= it },
-            down = { p += it }
-        )
-
-        return x * y
+        var x = 0; var y = 0; var p = 0
+        run({ x += it; y += (p * it) }, { p -= it }, { p += it }).also { return x * y }
     }
 
-    private fun run(forward: (Int) -> Unit, up: (Int) -> Unit, down: (Int) -> Unit) = actions.forEach {
+    private fun run(forward: Run, up: Run, down: Run) = actions.forEach {
         when (it.first) {
             "forward" -> forward(it.second)
             "up" -> up(it.second)
