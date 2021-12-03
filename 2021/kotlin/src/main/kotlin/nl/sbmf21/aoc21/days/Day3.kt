5058a4b3d3a0e360eb88778c8d3047bb37a37c1f
Boukje <boukje@switchware.io>
2021-12-03 08:50:16 +0100

    another refactor

--- 2021/kotlin/src/main/kotlin/nl/sbmf21/aoc21/days/Day3.kt
+++ 2021/kotlin/src/main/kotlin/nl/sbmf21/aoc21/days/Day3.kt
@@ -5,22 +5,23 @@ import nl.sbmf21.aoc21.Aoc
 
 class Day3(aoc: Aoc, number: Int) : ADay(aoc, number) {
 
-    override fun part1() = input[0].mapIndexed { i, _ -> count(i) }
-        .map { p -> Pair(c(p) { a, b -> a <= b }, c(p) { a, b -> a > b }) }
-        .fold(Pair("", "")) { a, p -> Pair(a.first + p.first, a.second + p.second) }
-        .run { combine(this) }
+    override fun part1() = input[0]
+        .mapIndexed { i, _ -> c(i) }
+        .map { p -> Pair(m(p) { a, b -> a <= b }, m(p) { a, b -> a > b }) }
+        .fold(Pair("", "")) { a, p -> Pair(a.first + p.first, a.second + p.second) }.run { combine(this) }
 
     override fun part2() = Pair(filter { a, b -> a <= b }[0], filter { a, b -> a > b }[0]).run { combine(this) }
 
-    private fun count(i: Int, l: List<String> = input) =
-        Pair(l.filter { it[i] == '0' }.size, l.filter { it[i] == '1' }.size)
+    private fun c(i: Int, l: List<String> = input) = Pair(
+        l.filter { it[i] == '0' }.size,
+        l.filter { it[i] == '1' }.size
+    )
 
-    private fun filter(l: List<String> = input, i: Int = 0, f: (a: Int, b: Int) -> Boolean): List<String> {
-        val filtered = l.filter { it[i] == c(count(i, l), f) }
-        return if (filtered.size > 1) filter(filtered, i + 1, f) else filtered
-    }
+    private fun filter(l: List<String> = input, i: Int = 0, f: (a: Int, b: Int) -> Boolean): List<String> = l
+        .filter { it[i] == m(c(i, l), f) }
+        .run { if (this.size > 1) filter(this, i + 1, f) else this }
 }
 
-private fun c(p: Pair<Int, Int>, f: (a: Int, b: Int) -> Boolean) = if (f(p.first, p.second)) '1' else '0'
+private fun m(p: Pair<Int, Int>, f: (a: Int, b: Int) -> Boolean) = if (f(p.first, p.second)) '1' else '0'
 private fun combine(p: Pair<String, String>) = parse(p.first) * parse(p.second)
 private fun parse(n: String) = Integer.parseInt(n, 2)
