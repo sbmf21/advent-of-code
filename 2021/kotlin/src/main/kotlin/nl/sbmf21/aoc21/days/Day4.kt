3ae4a115bcdcabe11e5c1ded257ccad96f1183cf
Boukje <boukje@switchware.io>
2021-12-03 17:40:32 +0100

    prepare for day 4

---
+++ 2021/kotlin/src/main/kotlin/nl/sbmf21/aoc21/days/Day4.kt
@@ -0,0 +1,11 @@
+package nl.sbmf21.aoc21.days
+
+import nl.sbmf21.aoc.common.ADay
+import nl.sbmf21.aoc21.Aoc
+
+class Day4(aoc: Aoc, number: Int) : ADay(aoc, number) {
+
+    override fun part1() = -1
+
+    override fun part2() = -1
+}
---
+++ 2021/kotlin/src/test/kotlin/Day4Test.kt
@@ -0,0 +1,15 @@
+import nl.sbmf21.aoc21.Aoc
+import nl.sbmf21.aoc21.days.Day4
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class Day4Test {
+
+    private var day = Day4(Aoc(), 4)
+
+    @Test
+    fun testPart1Output() = assertEquals(-1, day.part1())
+
+    @Test
+    fun testPart2Output() = assertEquals(-1, day.part2())
+}
