e5e08d580d836defb05bd20e4c5391ea009cd100
Boukje <boukje@switchware.io>
2021-12-03 09:01:08 +0100

    last refactor I swear

--- 2021/kotlin/src/main/kotlin/nl/sbmf21/aoc21/days/Day3.kt
+++ 2021/kotlin/src/main/kotlin/nl/sbmf21/aoc21/days/Day3.kt
@@ -6,20 +6,16 @@ import nl.sbmf21.aoc21.Aoc
 class Day3(aoc: Aoc, number: Int) : ADay(aoc, number) {
 
     override fun part1() = input[0]
-        .mapIndexed { i, _ -> c(i) }
-        .map { p -> Pair(m(p) { a, b -> a <= b }, m(p) { a, b -> a > b }) }
+        .mapIndexed { i, _ -> c(i) }.map { p -> Pair(m(p) { a, b -> a <= b }, m(p) { a, b -> a > b }) }
         .fold(Pair("", "")) { a, p -> Pair(a.first + p.first, a.second + p.second) }.run { combine(this) }
 
-    override fun part2() = Pair(filter { a, b -> a <= b }[0], filter { a, b -> a > b }[0]).run { combine(this) }
+    override fun part2() = Pair(f { a, b -> a <= b }[0], f { a, b -> a > b }[0]).run { combine(this) }
 
-    private fun c(i: Int, l: List<String> = input) = Pair(
-        l.filter { it[i] == '0' }.size,
-        l.filter { it[i] == '1' }.size
-    )
+    private fun c(i: Int, l: List<String> = input) =
+        Pair(l.filter { it[i] == '0' }.size, l.filter { it[i] == '1' }.size)
 
-    private fun filter(l: List<String> = input, i: Int = 0, f: (a: Int, b: Int) -> Boolean): List<String> = l
-        .filter { it[i] == m(c(i, l), f) }
-        .run { if (this.size > 1) filter(this, i + 1, f) else this }
+    private fun f(l: List<String> = input, i: Int = 0, f: (a: Int, b: Int) -> Boolean): List<String> =
+        l.filter { it[i] == m(c(i, l), f) }.run { if (this.size > 1) f(this, i + 1, f) else this }
 }
 
 private fun m(p: Pair<Int, Int>, f: (a: Int, b: Int) -> Boolean) = if (f(p.first, p.second)) '1' else '0'
