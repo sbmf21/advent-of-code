457cc9c51ff079a39d7e17a598df59dded7a5035
Boukje Steur <boukje@switchware.io>
2021-12-04 12:22:05 +0100

    make iterator inline

--- aoc-commons/src/main/kotlin/nl/sbmf21/aoc/common/Util.kt
+++ aoc-commons/src/main/kotlin/nl/sbmf21/aoc/common/Util.kt
@@ -1,6 +1,6 @@
 package nl.sbmf21.aoc.common
 
-fun <T> MutableList<T>.iterated(action: (MutableIterator<T>, T) -> Unit): MutableList<T> {
+inline fun <T> MutableList<T>.iterated(action: (MutableIterator<T>, T) -> Unit): MutableList<T> {
     val iterator = iterator()
 
     while (iterator.hasNext()) action(iterator, iterator.next())
