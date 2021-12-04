521f39d1fb3c0586dac95905c1b6ba1cd915ab74
Boukje Steur <boukje@switchware.io>
2021-12-04 13:45:05 +0100

    prepare for day 5

---
+++ 2021/kotlin/src/main/kotlin/nl/sbmf21/aoc21/days/Day5.kt
@@ -0,0 +1,10 @@
+package nl.sbmf21.aoc21.days
+
+import nl.sbmf21.aoc.common.ADay
+
+class Day5(input: List<String>) : ADay(input) {
+
+    override fun part1() = -1
+
+    override fun part2() = -1
+}
---
+++ 2021/kotlin/src/test/kotlin/Day5Test.kt
@@ -0,0 +1,11 @@
+import nl.sbmf21.aoc21.days.Day5
+import org.junit.jupiter.api.Test
+
+class Day5Test {
+
+    @Test
+    fun test() = testDay(Day5::class.java, -1, -1)
+
+    @Test
+    fun testExample() = testDay(Day5::class.java, -1, -1, true)
+}
