eb7ccaf1feb13de7a6962346b569a8aef6cb6a3e
Boukje <boukje@switchware.io>
2021-12-06 09:21:46 +0100

    clean up runner

--- 2021/kotlin/src/main/kotlin/nl/sbmf21/aoc21/Aoc.kt
+++ 2021/kotlin/src/main/kotlin/nl/sbmf21/aoc21/Aoc.kt
@@ -4,6 +4,4 @@ import nl.sbmf21.aoc.common.AocBase
 
 class Aoc : AocBase("2021")
 
-fun main(args: Array<String>) = Aoc().run {
-    exec(args)
-}
+fun main(args: Array<String>) = Aoc().run { exec(args) }
