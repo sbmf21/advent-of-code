c1e21e1647f33567d90f0a70652a0e27282f50a0
Boukje Steur <boukje@switchware.io>
2021-12-06 22:02:54 +0100

    update 2021 to commons 4

--- 2021/kotlin/src/test/kotlin/AocTest.kt
+++ 2021/kotlin/src/test/kotlin/AocTest.kt
@@ -1,5 +1,5 @@
 import nl.sbmf21.aoc.common.ADay
-import nl.sbmf21.aoc.common.buildDay
+import nl.sbmf21.aoc.common.DayMeta
 import nl.sbmf21.aoc21.Aoc
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
@@ -8,11 +8,11 @@ import kotlin.test.assertTrue
 class AocTest {
 
     @Test
-    fun names() = Aoc().findDays().forEach { day -> assertTrue(day.simpleName.matches(Regex("Day\\d+"))) }
+    fun names() = Aoc().days.forEach { day -> assertTrue(day.clazz.simpleName.matches(Regex("Day\\d+"))) }
 }
 
-fun testDay(cls: Class<out ADay>, part1: Number, part2: Number, example: Boolean = false) {
-    val day = buildDay(cls, example)
+fun <N : Number> testDay(clazz: Class<out ADay>, part1: N, part2: N, example: Boolean = false) {
+    val day = DayMeta(clazz).build(example)
 
     assertEquals(part1, day.part1())
     assertEquals(part2, day.part2())
