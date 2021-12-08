8c68f484c257bdab374508c057d17026288b6cc7
Boukje Steur <boukje@switchware.io>
2021-12-08 18:39:13 +0100

    allow custom input file and any return type

--- 2021/kotlin/src/test/kotlin/AocTest.kt
+++ 2021/kotlin/src/test/kotlin/AocTest.kt
@@ -1,18 +1,15 @@
 import nl.sbmf21.aoc.common.ADay
 import nl.sbmf21.aoc.common.DayMeta
-import nl.sbmf21.aoc21.Aoc
-import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
-import kotlin.test.assertTrue
 
-class AocTest {
-
-    @Test
-    fun names() = Aoc().days.forEach { day -> assertTrue(day.clazz.simpleName.matches(Regex("Day\\d+"))) }
-}
-
-fun <N : Number> testDay(clazz: Class<out ADay>, part1: N, part2: N, example: Boolean = false) {
-    val day = DayMeta(clazz).build(example)
+fun <N : Number> testDay(
+    clazz: Class<out ADay>,
+    part1: N,
+    part2: N,
+    example: Boolean = false,
+    filename: String? = null
+) {
+    val day = DayMeta(clazz).build(example, filename)
 
     assertEquals(part1, day.part1())
     assertEquals(part2, day.part2())
--- aoc-commons/src/main/kotlin/nl/sbmf21/aoc/common/DayMeta.kt
+++ aoc-commons/src/main/kotlin/nl/sbmf21/aoc/common/DayMeta.kt
@@ -3,12 +3,12 @@ package nl.sbmf21.aoc.common
 data class DayMeta<T : ADay>(val clazz: Class<out T>) {
     val number = clazz.simpleName.substring("Day".length).toInt()
 
-    fun build(example: Boolean = false): T = clazz
-        .getDeclaredConstructor(List::class.java).newInstance(file(example))
+    fun build(example: Boolean = false, filename: String? = null): T = clazz
+        .getDeclaredConstructor(List::class.java).newInstance(file(example, filename))
         .also { it.number = this.number }
 
-    private fun file(example: Boolean) = this::class.java
-        .getResourceAsStream("/${if (example) "example" else "input"}/day$number.txt")!!
+    private fun file(example: Boolean, filename: String?) = this::class.java
+        .getResourceAsStream("/${if (example) "example" else "input"}/day$number${if (filename == null) "" else "-$filename"}.txt")!!
         .bufferedReader()
         .lines()
         .toArray()
