1eda8f9caad99769359823526869f855e3f5eb0b
Boukje <boukje@switchware.io>
2021-12-10 08:51:40 +0100

    prepare for day 11

---
+++ 2021/kotlin/src/main/kotlin/nl/sbmf21/aoc21/days/Day11.kt
@@ -0,0 +1,16 @@
+package nl.sbmf21.aoc21.days
+
+import nl.sbmf21.aoc.common.ADay
+
+class Day11(input: List<String>) : ADay(input) {
+
+    override fun part1(): Any {
+
+        return "none"
+    }
+
+    override fun part2(): Any {
+
+        return "none"
+    }
+}
