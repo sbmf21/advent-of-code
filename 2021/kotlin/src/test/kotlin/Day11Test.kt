3a8461a953ef2e8c2259fe17b7c5e095cfa0da8b
Boukje <boukje@switchware.io>
2021-12-10 08:51:40 +0100

    prepare for day 11

---
+++ 2021/kotlin/src/test/kotlin/Day11Test.kt
@@ -0,0 +1,11 @@
+import nl.sbmf21.aoc21.days.Day11
+import org.junit.jupiter.api.Test
+
+class Day11Test {
+
+    @Test
+    fun testInput() = testDay(Day11::class.java, "none", "none")
+
+    @Test
+    fun testExample() = testDay(Day11::class.java, "none", "none", true)
+}
