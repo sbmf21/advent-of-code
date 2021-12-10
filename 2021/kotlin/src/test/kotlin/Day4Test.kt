85208d3ccb74096d477ac1f40074977c2a528658
Boukje <boukje@switchware.io>
2021-12-10 08:30:38 +0100

    clean up tests

--- 2021/kotlin/src/test/kotlin/Day4Test.kt
+++ 2021/kotlin/src/test/kotlin/Day4Test.kt
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test
 class Day4Test {
 
     @Test
-    fun testInput() = testDay(Day4::class.java, 55770, 2980)
+    fun testInput() = testDay(Day4::class.java, 55_770, 2980)
 
     @Test
     fun testExample() = testDay(Day4::class.java, 4512, 1924, true)
--- 2021/kotlin/src/test/kotlin/Day5Test.kt
+++ 2021/kotlin/src/test/kotlin/Day5Test.kt
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test
 class Day5Test {
 
     @Test
-    fun testInput() = testDay(Day5::class.java, 7318, 19939)
+    fun testInput() = testDay(Day5::class.java, 7318, 19_939)
 
     @Test
     fun testExample() = testDay(Day5::class.java, 5, 12, true)
