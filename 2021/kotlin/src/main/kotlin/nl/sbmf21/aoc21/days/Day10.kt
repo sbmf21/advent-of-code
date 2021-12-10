d622e9d0fc799f92e0ec723b021d89c6e88eb4c8
Boukje <boukje@switchware.io>
2021-12-10 07:07:16 +0100

    clean up more

--- 2021/kotlin/src/main/kotlin/nl/sbmf21/aoc21/days/Day10.kt
+++ 2021/kotlin/src/main/kotlin/nl/sbmf21/aoc21/days/Day10.kt
@@ -8,28 +8,12 @@ class Day10(input: List<String>) : ADay(input) {
     private val closing = mapOf('(' to ')', '[' to ']', '{' to '}', '<' to '>')
 
     override fun part1() = run({ it }, { null }).sorted()
-        .fold(mutableMapOf(')' to 0, ']' to 0, '}' to 0, '>' to 0)) { acc, c -> acc[c] = acc[c]!! + 1; acc }.map {
-            it.value * when (it.key) {
-                ')' -> 3
-                ']' -> 57
-                '}' -> 1197
-                '>' -> 25137
-                else -> 0
-            }
-        }.sum()
+        .fold(mutableMapOf(')' to 0, ']' to 0, '}' to 0, '>' to 0)) { acc, c -> acc[c] = acc[c]!! + 1; acc }
+        .map { it.value * points(it.key, 3, 57, 1197, 25137) }
+        .sum()
 
     override fun part2() = run({ null }, { it })
-        .map {
-            it.reversed().fold(0L) { acc, c ->
-                acc * 5 + when (closing[c]) {
-                    ')' -> 1
-                    ']' -> 2
-                    '}' -> 3
-                    '>' -> 4
-                    else -> 0
-                }
-            }
-        }
+        .map { it.reversed().fold(0L) { acc, c -> acc * 5 + points(closing[c], 1, 2, 3, 4) } }
         .sorted().run { this[size / 2] }
 
     private fun <C> run(c: (c: Char) -> C?, b: (b: List<Char>) -> C?): List<C> = input.map { line ->
@@ -43,4 +27,12 @@ class Day10(input: List<String>) : ADay(input) {
 
         return@map b(blocks.toList())
     }.filterNotNull()
+
+    private fun points(c: Char?, paren: Int, square: Int, curly: Int, angle: Int) = when (c) {
+        ')' -> paren
+        ']' -> square
+        '}' -> curly
+        '>' -> angle
+        else -> 0
+    }
 }
