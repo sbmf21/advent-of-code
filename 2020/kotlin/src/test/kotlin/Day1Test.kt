61f4a56c3878c396bd0ea0a7fa5a6c1c5dad974c
Boukje Steur <boukje@switchware.io>
2021-12-11 05:55:36 +0100

    add some example tests

--- 2020/kotlin/src/test/kotlin/Day1Test.kt
+++ 2020/kotlin/src/test/kotlin/Day1Test.kt
@@ -5,4 +5,7 @@ class Day1Test {
 
     @Test
     fun testInput() = testDay(Day1::class.java, 1_019_904, 176_647_680)
+
+    @Test
+    fun testExample() = testDay(Day1::class.java, 514_579, 241_861_950, true)
 }
