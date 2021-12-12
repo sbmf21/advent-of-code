09613a9b846dab9dfe56d0d4cbe040be88b7f151
Boukje Steur <boukje@switchware.io>
2021-12-12 16:40:46 +0100

    []=[y=2020,d=18]

---
+++ 2020/kotlin/src/main/kotlin/nl/sbmf21/aoc20/days/Day18.kt
@@ -0,0 +1,79 @@
+package nl.sbmf21.aoc20.days
+
+import nl.sbmf21.aoc.common.ADay
+import nl.sbmf21.aoc.common.iterated
+
+class Day18(input: List<String>) : ADay(input) {
+
+    private val expressions = input.map { line ->
+        line.toCharArray().filter { it != ' ' }.fold(Block()) { acc, xpr ->
+            when (xpr) {
+                '(' -> Block(acc).also { acc.addXpr(it) }
+                ')' -> acc.parent!!
+                else -> acc.also { acc.addXpr(xpr) }
+            }
+        }
+    }
+
+    override fun part1() = run()
+
+    override fun part2() = run(true)
+
+    private fun run(order: Boolean = false) = expressions.sumOf { it.exec(order).value }
+}
+
+internal class Block(val parent: Block? = null) : Expr {
+
+    private val parts = mutableListOf<Expr>()
+
+    fun addXpr(xpr: Any) = parts.add(
+        when (xpr) {
+            '+' -> Plus()
+            '*' -> Times()
+            is Block -> xpr
+            else -> ValExpr("$xpr".toLong())
+        }
+    )
+
+    fun exec(order: Boolean): ValExpr {
+        val parts = parts.map { e -> if (e is Block) e.exec(order) else e }.toMutableList()
+
+        if (order) parts.filterIsInstance<ExecExpr>().filter { it.first() }.forEach { e ->
+            val i = parts.indexOf(e) - 1
+            val pref = parts[i] as ValExpr
+            val next = parts[i + 2] as ValExpr
+
+            parts.removeAll(listOf(pref, e, next))
+            parts.add(i, e.exec(pref, next))
+        }
+
+        var value: ValExpr = parts.first() as ValExpr
+
+        parts.filterIndexed { i, _ -> i > 0 }
+            .toMutableList()
+            .iterated { i, e -> if (e is ExecExpr) value = e.exec(value, i.next() as ValExpr) }
+
+        return value
+    }
+}
+
+internal interface Expr
+internal class ValExpr(val value: Long) : Expr {
+    operator fun plus(v: ValExpr) = ValExpr(value + v.value)
+    operator fun times(v: ValExpr) = ValExpr(value * v.value)
+}
+
+internal interface ExecExpr : Expr {
+    fun exec(prev: ValExpr, next: ValExpr): ValExpr
+    fun first(): Boolean
+}
+
+internal class Plus : ExecExpr {
+    override fun exec(prev: ValExpr, next: ValExpr) = prev + next
+    override fun first() = true
+}
+
+internal class Times : ExecExpr {
+    override fun exec(prev: ValExpr, next: ValExpr) = prev * next
+    override fun first() = false
+}
