18e23247abd25e245f35626295bc3171fad6ed1b
Boukje Steur <boukje@switchware.io>
2021-12-12 11:03:30 +0100

    refactor a bit smaller

--- 2021/kotlin/src/main/kotlin/nl/sbmf21/aoc21/days/Day12.kt
+++ 2021/kotlin/src/main/kotlin/nl/sbmf21/aoc21/days/Day12.kt
@@ -13,24 +13,22 @@ class Day12(input: List<String>) : ADay(input) {
             if (acc.none { it == route.first }) acc.add(route.first)
             if (acc.none { it == route.second }) acc.add(route.second)
 
-            acc.first { it == route.first }.options.add(acc.first { it == route.second })
-            acc
+            acc.first { it == route.first }.options.add(acc.first { it == route.second }); acc
         }
 
     override fun part1() = mapCaves().size
 
-    override fun part2() = options
-        .filter { it.small }
+    override fun part2() = options.filter { !it.fat }
         .fold(mutableSetOf<String>()) { acc, cave -> acc.addAll(mapCaves(cave).map { it.toString() }); acc }.size
 
-    private fun mapCaves(isBig: Cave? = null): MutableList<Path> {
+    private fun mapCaves(fatCave: Cave? = null): MutableList<Path> {
         val paths = mutableListOf<Path>()
         val queue = mutableListOf(Path(options.first { it.cave == "start" }))
 
         while (queue.isNotEmpty()) queue.removeLast().run {
-            last.options
-                .filter { !it.small || !contains(it) || (it == isBig && containsTimes(it) < 2) }
-                .forEach { val path = next(it); (if (path.finished) paths else queue).add(path) }
+            steps.last().options
+                .filter { it.fat || !contains(it) || (it == fatCave && containsTimes(it) < 2) }
+                .forEach { val path = next(it); (if (path.steps.last().cave == "end") paths else queue).add(path) }
         }
 
         return paths
@@ -38,10 +36,7 @@ class Day12(input: List<String>) : ADay(input) {
 }
 
 internal class Path(cave: Cave? = null) {
-
-    private val steps = mutableListOf<Cave>().also { if (cave != null) it.add(cave) }
-    val last get() = steps.last()
-    val finished get() = last.cave == "end"
+    val steps = mutableListOf<Cave>().also { if (cave != null) it.add(cave) }
 
     fun contains(cave: Cave) = steps.contains(cave)
     fun containsTimes(cave: Cave) = steps.filter { it == cave }.size
@@ -50,9 +45,8 @@ internal class Path(cave: Cave? = null) {
 }
 
 internal data class Cave(val cave: String) {
-
     val options = mutableSetOf<Cave>()
-    val small = cave[0].isLowerCase()
+    val fat = cave[0].isUpperCase()
 
     override fun equals(other: Any?) = other is Cave && other.cave == cave
     override fun hashCode() = cave.hashCode()
