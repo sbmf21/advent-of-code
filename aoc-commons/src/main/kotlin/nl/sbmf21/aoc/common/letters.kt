0e908aea14c0ed0d64db3716d5739684016e56ee
Boukje <boukje@switchware.io>
2021-12-13 13:41:40 +0100

    Add more letters
    Only missing D, I, M, N, Q, T, V, W, X  
    (which is 36% still)

--- aoc-commons/src/main/kotlin/nl/sbmf21/aoc/common/letters.kt
+++ aoc-commons/src/main/kotlin/nl/sbmf21/aoc/common/letters.kt
@@ -3,128 +3,181 @@ package nl.sbmf21.aoc.common
 val AOC_LETTERS = listOf(
     Pair(
         'A', listOf(
-            listOf(0, 1, 1, 0),
-            listOf(1, 0, 0, 1),
-            listOf(1, 0, 0, 1),
-            listOf(1, 1, 1, 1),
-            listOf(1, 0, 0, 1),
-            listOf(1, 0, 0, 1),
+            listOf(0, 1, 1, 0), //  ██
+            listOf(1, 0, 0, 1), // █  █
+            listOf(1, 0, 0, 1), // █  █
+            listOf(1, 1, 1, 1), // ████
+            listOf(1, 0, 0, 1), // █  █
+            listOf(1, 0, 0, 1), // █  █
         )
     ),
     Pair(
         'B', listOf(
-            listOf(1, 1, 1, 0),
-            listOf(1, 0, 0, 1),
-            listOf(1, 1, 1, 0),
-            listOf(1, 0, 0, 1),
-            listOf(1, 0, 0, 1),
-            listOf(1, 1, 1, 0),
+            listOf(1, 1, 1, 0), // ███
+            listOf(1, 0, 0, 1), // █  █
+            listOf(1, 1, 1, 0), // ███
+            listOf(1, 0, 0, 1), // █  █
+            listOf(1, 0, 0, 1), // █  █
+            listOf(1, 1, 1, 0), // ███
         )
     ),
     Pair(
         'C', listOf(
-            listOf(0, 1, 1, 0),
-            listOf(1, 0, 0, 1),
-            listOf(1, 0, 0, 0),
-            listOf(1, 0, 0, 0),
-            listOf(1, 0, 0, 1),
-            listOf(0, 1, 1, 0),
+            listOf(0, 1, 1, 0), //  ██
+            listOf(1, 0, 0, 1), // █  █
+            listOf(1, 0, 0, 0), // █
+            listOf(1, 0, 0, 0), // █
+            listOf(1, 0, 0, 1), // █  █
+            listOf(0, 1, 1, 0), //  ██
         )
     ),
-    // D E F
+    Pair('D', listOf()), // █ UNKNOWN █
+    Pair(
+        'E', listOf(
+            listOf(1, 1, 1, 1), // ████
+            listOf(1, 0, 0, 0), // █
+            listOf(1, 1, 1, 0), // ███
+            listOf(1, 0, 0, 0), // █
+            listOf(1, 0, 0, 0), // █
+            listOf(1, 1, 1, 1), // ████
+        )
+    ),
+    Pair(
+        'F', listOf(
+            listOf(1, 1, 1, 1), // ████
+            listOf(1, 0, 0, 0), // █
+            listOf(1, 1, 1, 0), // ███
+            listOf(1, 0, 0, 0), // █
+            listOf(1, 0, 0, 0), // █
+            listOf(1, 0, 0, 0), // █
+        )
+    ),
     Pair(
         'G', listOf(
-            listOf(0, 1, 1, 0),
-            listOf(1, 0, 0, 1),
-            listOf(1, 0, 0, 0),
-            listOf(1, 0, 1, 1),
-            listOf(1, 0, 0, 1),
-            listOf(0, 1, 1, 1),
+            listOf(0, 1, 1, 0), //  ██
+            listOf(1, 0, 0, 1), // █  █
+            listOf(1, 0, 0, 0), // █
+            listOf(1, 0, 1, 1), // █ ██
+            listOf(1, 0, 0, 1), // █  █
+            listOf(0, 1, 1, 1), //  ███
         )
     ),
     Pair(
         'H', listOf(
-            listOf(1, 0, 0, 1),
-            listOf(1, 0, 0, 1),
-            listOf(1, 1, 1, 1),
-            listOf(1, 0, 0, 1),
-            listOf(1, 0, 0, 1),
-            listOf(1, 0, 0, 1),
+            listOf(1, 0, 0, 1), // █  █
+            listOf(1, 0, 0, 1), // █  █
+            listOf(1, 1, 1, 1), // ████
+            listOf(1, 0, 0, 1), // █  █
+            listOf(1, 0, 0, 1), // █  █
+            listOf(1, 0, 0, 1), // █  █
         )
     ),
-    // I
+    Pair('I', listOf()), // █ UNKNOWN █
     Pair(
         'J', listOf(
-            listOf(0, 0, 1, 1),
-            listOf(0, 0, 0, 1),
-            listOf(0, 0, 0, 1),
-            listOf(0, 0, 0, 1),
-            listOf(1, 0, 0, 1),
-            listOf(0, 1, 1, 0),
+            listOf(0, 0, 1, 1), //   ██
+            listOf(0, 0, 0, 1), //    █
+            listOf(0, 0, 0, 1), //    █
+            listOf(0, 0, 0, 1), //    █
+            listOf(1, 0, 0, 1), // █  █
+            listOf(0, 1, 1, 0), //  ██
         )
     ),
     Pair(
         'K', listOf(
-            listOf(1, 0, 0, 1),
-            listOf(1, 0, 1, 0),
-            listOf(1, 1, 0, 0),
-            listOf(1, 0, 1, 0),
-            listOf(1, 0, 1, 0),
-            listOf(1, 0, 0, 1),
+            listOf(1, 0, 0, 1), // █  █
+            listOf(1, 0, 1, 0), // █ █
+            listOf(1, 1, 0, 0), // ██
+            listOf(1, 0, 1, 0), // █ █
+            listOf(1, 0, 1, 0), // █ █
+            listOf(1, 0, 0, 1), // █  █
         )
     ),
     Pair(
         'L', listOf(
-            listOf(1, 0, 0, 0),
-            listOf(1, 0, 0, 0),
-            listOf(1, 0, 0, 0),
-            listOf(1, 0, 0, 0),
-            listOf(1, 0, 0, 0),
-            listOf(1, 1, 1, 1),
+            listOf(1, 0, 0, 0), // █
+            listOf(1, 0, 0, 0), // █
+            listOf(1, 0, 0, 0), // █
+            listOf(1, 0, 0, 0), // █
+            listOf(1, 0, 0, 0), // █
+            listOf(1, 1, 1, 1), // ████
         )
     ),
-    // M N O
+    Pair('M', listOf()), // █ UNKNOWN █
+    Pair('N', listOf()), // █ UNKNOWN █
+    Pair(
+        'O', listOf(
+            listOf(0, 1, 1, 0), //  ██
+            listOf(1, 0, 0, 1), // █  █
+            listOf(1, 0, 0, 1), // █  █
+            listOf(1, 0, 0, 1), // █  █
+            listOf(1, 0, 0, 1), // █  █
+            listOf(0, 1, 1, 0), //  ██
+        )
+    ),
     Pair(
         'P', listOf(
-            listOf(1, 1, 1, 0),
-            listOf(1, 0, 0, 1),
-            listOf(1, 0, 0, 1),
-            listOf(1, 1, 1, 0),
-            listOf(1, 0, 0, 0),
-            listOf(1, 0, 0, 0),
+            listOf(1, 1, 1, 0), // ███
+            listOf(1, 0, 0, 1), // █  █
+            listOf(1, 0, 0, 1), // █  █
+            listOf(1, 1, 1, 0), // ███
+            listOf(1, 0, 0, 0), // █
+            listOf(1, 0, 0, 0), // █
         )
     ),
-    // Q
+    Pair('Q', listOf()), // █ UNKNOWN █
     Pair(
         'R', listOf(
-            listOf(1, 1, 1, 0),
-            listOf(1, 0, 0, 1),
-            listOf(1, 0, 0, 1),
-            listOf(1, 1, 1, 0),
-            listOf(1, 0, 1, 0),
-            listOf(1, 0, 0, 1),
+            listOf(1, 1, 1, 0), // ███
+            listOf(1, 0, 0, 1), // █  █
+            listOf(1, 0, 0, 1), // █  █
+            listOf(1, 1, 1, 0), // ███
+            listOf(1, 0, 1, 0), // █ █
+            listOf(1, 0, 0, 1), // █  █
         )
     ),
-    // S T
+    Pair(
+        'S', listOf(
+            listOf(0, 1, 1, 1), //  ███
+            listOf(1, 0, 0, 0), // █
+            listOf(1, 0, 0, 0), // █
+            listOf(0, 1, 1, 0), //  ██
+            listOf(0, 0, 0, 1), //    █
+            listOf(1, 1, 1, 0), // ███
+        )
+    ),
+    Pair('T', listOf()), // █ UNKNOWN █
     Pair(
         'U', listOf(
-            listOf(1, 0, 0, 1),
-            listOf(1, 0, 0, 1),
-            listOf(1, 0, 0, 1),
-            listOf(1, 0, 0, 1),
-            listOf(1, 0, 0, 1),
-            listOf(0, 1, 1, 0),
+            listOf(1, 0, 0, 1), // █  █
+            listOf(1, 0, 0, 1), // █  █
+            listOf(1, 0, 0, 1), // █  █
+            listOf(1, 0, 0, 1), // █  █
+            listOf(1, 0, 0, 1), // █  █
+            listOf(0, 1, 1, 0), //  ██
         )
     ),
-    // V W X Y
+    Pair('V', listOf()), // █ UNKNOWN █
+    Pair('W', listOf()), // █ UNKNOWN █
+    Pair('X', listOf()), // █ UNKNOWN █
+    Pair( // FIVE WIDE?????
+        'Y', listOf(
+            listOf(1, 0, 0, 0, 1), // █   █
+            listOf(1, 0, 0, 0, 1), // █   █
+            listOf(0, 1, 0, 1, 0), //  █ █
+            listOf(0, 0, 1, 0, 0), //   █
+            listOf(0, 0, 1, 0, 0), //   █
+            listOf(0, 0, 1, 0, 0), //   █
+        )
+    ),
     Pair(
         'Z', listOf(
-            listOf(1, 1, 1, 1),
-            listOf(0, 0, 0, 1),
-            listOf(0, 0, 1, 0),
-            listOf(0, 1, 0, 0),
-            listOf(1, 0, 0, 0),
-            listOf(1, 1, 1, 1),
+            listOf(1, 1, 1, 1), // ████
+            listOf(0, 0, 0, 1), //    █
+            listOf(0, 0, 1, 0), //   █
+            listOf(0, 1, 0, 0), //  █
+            listOf(1, 0, 0, 0), // █
+            listOf(1, 1, 1, 1), // ████
         )
     ),
 )
