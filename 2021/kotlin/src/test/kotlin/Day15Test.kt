24759ab3e63044447df73779cdc3ca2059ef0310
Boukje <boukje@switchware.io>
2021-12-15 12:06:14 +0100

    []=[y=2021,d=15,p=1]

---
+++ 2021/kotlin/src/test/kotlin/Day15Test.kt
@@ -0,0 +1,11 @@
+import nl.sbmf21.aoc21.days.Day15
+import org.junit.jupiter.api.Test
+
+class Day15Test {
+
+    @Test
+    fun testInput() = testDay(Day15::class.java, 769, -1)
+
+    @Test
+    fun testExample() = testDay(Day15::class.java, 40, -1, true)
+}
