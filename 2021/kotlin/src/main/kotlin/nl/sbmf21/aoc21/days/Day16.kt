6e4e4cc245257545103c4b954f9c1e1061b5bb9d
Boukje <boukje@switchware.io>
2021-12-16 10:14:09 +0100

    removed 54 lines owo

--- 2021/kotlin/src/main/kotlin/nl/sbmf21/aoc21/days/Day16.kt
+++ 2021/kotlin/src/main/kotlin/nl/sbmf21/aoc21/days/Day16.kt
@@ -5,15 +5,13 @@ import nl.sbmf21.aoc.common.ADay
 class Day16(input: List<String>) : ADay(input) {
 
     private val initial = input[0].map { Integer.toBinaryString("$it".toInt(16)).padStart(4, '0') }.joinToString("")
+    private val packet = decryptPacket()
 
-    override fun part1() = getVersion(decryptPacket())
+    override fun part1() = packet.version()
 
-    override fun part2() = decryptPacket().exec()
+    override fun part2() = packet.exec()
 
-    private fun getVersion(packet: Packet<Any>): Int =
-        (if (packet is OperatorPacket) packet.subPackets.sumOf { getVersion(it) } else 0) + packet.version
-
-    private fun decryptPacket(line: String = initial): Packet<Any> {
+    private fun decryptPacket(line: String = initial): BitsPacket {
         val version = line.substring(0, 3).toInt(2)
         val typeId = line.substring(3, 6).toInt(2)
         var index = 6
@@ -27,102 +25,50 @@ class Day16(input: List<String>) : ADay(input) {
                 if (start == "0") break
             }
 
-            return LiteralPacket(version, number.bin(), index)
+            return BitsPacket(version, typeId, index, value = number.toLong(2))
         } else {
-            val subPackets = mutableListOf<Packet<Any>>()
-            var nextIndex = ++index
+            val packets = mutableListOf<BitsPacket>()
+            var next = ++index
 
             if (line[6] == '0') {
-                nextIndex += 15
-                val end = nextIndex + line.substring(index, nextIndex).toInt(2)
-                while (nextIndex < end) nextIndex += add(line, nextIndex, subPackets)
+                next += 15
+                val end = next + line.substring(index, next).toInt(2)
+                while (next < end) next += add(line, next, packets)
             } else {
-                nextIndex += 11
-                for (i in 0 until line.substring(index, nextIndex).bin()) nextIndex += add(line, nextIndex, subPackets)
+                next += 11
+                for (i in 0 until line.substring(index, next).toLong(2)) next += add(line, next, packets)
             }
 
-            return when (typeId) {
-                0 -> SumPacket(version, subPackets, nextIndex)
-                1 -> ProductPacket(version, subPackets, nextIndex)
-                2 -> MinimumPacket(version, subPackets, nextIndex)
-                3 -> MaximumPacket(version, subPackets, nextIndex)
-                5 -> GreaterThanPacket(version, subPackets, nextIndex)
-                6 -> LessThanPacket(version, subPackets, nextIndex)
-                7 -> EqualToPacket(version, subPackets, nextIndex)
-                else -> throw IllegalStateException()
-            }
+            return BitsPacket(version, typeId, next, packets = packets)
         }
     }
 
-    private fun add(line: String, nextIndex: Int, subPackets: MutableList<Packet<Any>>) =
-        decryptPacket(line.substring(nextIndex)).run { subPackets.add(this); lastIndex }
+    private fun add(line: String, nextIndex: Int, subPackets: MutableList<BitsPacket>) =
+        decryptPacket(line.substring(nextIndex)).run { subPackets.add(this); end }
 }
 
-internal fun String.bin() = toLong(2)
-
-internal abstract class Packet<out T : Any>(val version: Int, val value: T, val lastIndex: Int) {
-    abstract fun exec(): Long
-}
-
-internal class LiteralPacket(version: Int, private val number: Long, lastIndex: Int) :
-    Packet<Number>(version, number, lastIndex) {
-
-    override fun exec() = number
-}
-
-internal abstract class OperatorPacket(version: Int, val subPackets: List<Packet<Any>>, lastIndex: Int) :
-    Packet<List<Packet<Any>>>(version, subPackets, lastIndex)
-
-internal class SumPacket(version: Int, subPackets: List<Packet<Any>>, lastIndex: Int) :
-    OperatorPacket(version, subPackets, lastIndex) {
-
-    override fun exec() = subPackets.sumOf { it.exec() }
-}
-
-internal class ProductPacket(version: Int, subPackets: List<Packet<Any>>, lastIndex: Int) :
-    OperatorPacket(version, subPackets, lastIndex) {
-
-    override fun exec() = subPackets.map { it.exec() }.fold(1L) { acc, p -> acc * p }
-}
-
-internal class MinimumPacket(version: Int, subPackets: List<Packet<Any>>, lastIndex: Int) :
-    OperatorPacket(version, subPackets, lastIndex) {
-
-    override fun exec() = subPackets.minOf { it.exec() }
-}
-
-internal class MaximumPacket(version: Int, subPackets: List<Packet<Any>>, lastIndex: Int) :
-    OperatorPacket(version, subPackets, lastIndex) {
-
-    override fun exec() = subPackets.maxOf { it.exec() }
-}
-
-internal abstract class DualOperatorPacket(version: Int, subPackets: List<Packet<Any>>, lastIndex: Int) :
-    OperatorPacket(version, subPackets, lastIndex) {
-
-    override fun exec(): Long {
-        if (subPackets.size != 2) throw IllegalStateException("Found illegal sub packet size")
-
-        return if (calc(subPackets[0].exec(), subPackets[1].exec())) 1 else 0
+internal class BitsPacket(
+    private val version: Int,
+    private val typeId: Int,
+    val end: Int,
+    private val packets: List<BitsPacket> = listOf(),
+    private val value: Long = 0L,
+) {
+    fun exec(): Long = packets.map { it.exec() }.run {
+        when (typeId) {
+            0 -> sum()
+            1 -> fold(1L) { acc, p -> acc * p }
+            2 -> minOf { it }
+            3 -> maxOf { it }
+            4 -> value
+            5 -> (this[0] > this[1]).toLong()
+            6 -> (this[0] < this[1]).toLong()
+            7 -> (this[0] == this[1]).toLong()
+            else -> 0L
+        }
     }
 
-    abstract fun calc(left: Long, right: Long): Boolean;
+    fun version(): Int = packets.sumOf { it.version() } + version
 }
 
-internal class GreaterThanPacket(version: Int, subPackets: List<Packet<Any>>, lastIndex: Int) :
-    DualOperatorPacket(version, subPackets, lastIndex) {
-
-    override fun calc(left: Long, right: Long) = left > right
-}
-
-internal class LessThanPacket(version: Int, subPackets: List<Packet<Any>>, lastIndex: Int) :
-    DualOperatorPacket(version, subPackets, lastIndex) {
-
-    override fun calc(left: Long, right: Long) = left < right
-}
-
-internal class EqualToPacket(version: Int, subPackets: List<Packet<Any>>, lastIndex: Int) :
-    DualOperatorPacket(version, subPackets, lastIndex) {
-
-    override fun calc(left: Long, right: Long) = left == right
-}
+internal fun Boolean.toLong() = if (this) 1L else 0L
