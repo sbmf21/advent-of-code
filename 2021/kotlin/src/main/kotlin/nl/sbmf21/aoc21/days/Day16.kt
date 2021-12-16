ea5e8b53cad0303b7c36ab7e94e6c76df3f30220
Boukje <boukje@switchware.io>
2021-12-16 12:17:32 +0100

    ++speed

--- 2021/kotlin/src/main/kotlin/nl/sbmf21/aoc21/days/Day16.kt
+++ 2021/kotlin/src/main/kotlin/nl/sbmf21/aoc21/days/Day16.kt
@@ -4,56 +4,42 @@ import nl.sbmf21.aoc.common.ADay
 
 class Day16(input: List<String>) : ADay(input) {
 
-    private val initial = input[0].map { Integer.toBinaryString("$it".toInt(16)).padStart(4, '0') }.joinToString("")
-    private val packet = decryptPacket()
+    private val packet = BitsPacket(input[0].map { "$it".toInt(16).toString(2).padStart(4, '0') }.joinToString(""))
 
     override fun part1() = packet.version()
-
     override fun part2() = packet.exec()
-
-    private fun decryptPacket(line: String = initial): BitsPacket {
-        val version = line.substring(0, 3).toInt(2)
-        val typeId = line.substring(3, 6).toInt(2)
-        var index = 6
-
-        if (typeId == 4) {
-            var number = ""
-
-            while (true) {
-                val start = line.substring(index, index + 1)
-                number += line.substring(index + 1, index + 5); index += 5
-                if (start == "0") break
-            }
-
-            return BitsPacket(version, typeId, index, value = number.toLong(2))
-        } else {
-            val packets = mutableListOf<BitsPacket>()
-            var next = ++index
-
-            if (line[6] == '0') {
-                next += 15
-                val end = next + line.substring(index, next).toInt(2)
-                while (next < end) next += add(line, next, packets)
-            } else {
-                next += 11
-                for (i in 0 until line.substring(index, next).toLong(2)) next += add(line, next, packets)
-            }
-
-            return BitsPacket(version, typeId, next, packets = packets)
-        }
-    }
-
-    private fun add(line: String, nextIndex: Int, subPackets: MutableList<BitsPacket>) =
-        decryptPacket(line.substring(nextIndex)).run { subPackets.add(this); end }
 }
 
-internal class BitsPacket(
-    private val version: Int,
-    private val typeId: Int,
-    val end: Int,
-    private val packets: List<BitsPacket> = listOf(),
-    private val value: Long = 0L,
-) {
+internal class BitsPacket(input: String, start: Int = 0) {
+    private val version = input.substring(start, start + 3).toInt(2)
+    private val typeId = input.substring(start + 3, start + 6).toInt(2)
+    private val length: Int
+    private val packets = mutableListOf<BitsPacket>()
+    private var value = 0L
+
+    init {
+        var length = start + 6
+
+        if (typeId == 4) value = input.substring(length).chunked(5)
+            .run { subList(0, indexOfFirst { it[0] == '0' } + 1).joinToString("") { length += 5; it.substring(1) } }
+            .toLong(2)
+        else length = (length++).run {
+            var next = this
+            if (input[next++] == '0') {
+                next += 15
+                val end = next + input.substring(length, next).toInt(2)
+                while (next < end) next = add(input, next)
+            } else {
+                next += 11
+                for (i in 0 until input.substring(length, next).toLong(2)) next = add(input, next)
+            }; next
+        }
+
+        this.length = length
+    }
+
+    private fun add(input: String, next: Int) = BitsPacket(input, next).also { packets.add(it) }.length
+
     fun exec(): Long = packets.map { it.exec() }.run {
         when (typeId) {
             0 -> sum()
@@ -61,14 +47,12 @@ internal class BitsPacket(
             2 -> minOf { it }
             3 -> maxOf { it }
             4 -> value
-            5 -> (this[0] > this[1]).toLong()
-            6 -> (this[0] < this[1]).toLong()
-            7 -> (this[0] == this[1]).toLong()
+            5 -> if (this[0] > this[1]) 1L else 0L
+            6 -> if (this[0] < this[1]) 1L else 0L
+            7 -> if (this[0] == this[1]) 1L else 0L
             else -> 0L
         }
     }
 
     fun version(): Int = packets.sumOf { it.version() } + version
 }
-
-internal fun Boolean.toLong() = if (this) 1L else 0L
