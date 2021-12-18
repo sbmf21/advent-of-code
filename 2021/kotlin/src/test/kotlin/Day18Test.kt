56d1e3398ae92b5a5b6f65522c5e0872d904da73
Boukje Steur <boukje@switchware.io>
2021-12-18 11:57:18 +0100

    []=[y=2021,d=18]

---
+++ 2021/kotlin/src/test/kotlin/Day18Test.kt
@@ -0,0 +1,11 @@
+import nl.sbmf21.aoc21.days.Day18
+import org.junit.jupiter.api.Test
+
+class Day18Test {
+
+    @Test
+    fun testInput() = testDay(Day18::class.java, 3981, 4687)
+
+    @Test
+    fun testExample() = testDay(Day18::class.java, 4140, 3993, true)
+}
