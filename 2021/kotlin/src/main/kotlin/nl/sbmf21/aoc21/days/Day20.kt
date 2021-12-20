3f0cfb98f5391b2140472b42c6460a016f517d06
Boukje <boukje@switchware.io>
2021-12-20 07:31:34 +0100

    clean up more

--- 2021/kotlin/src/main/kotlin/nl/sbmf21/aoc21/days/Day20.kt
+++ 2021/kotlin/src/main/kotlin/nl/sbmf21/aoc21/days/Day20.kt
@@ -1,23 +1,22 @@
 package nl.sbmf21.aoc21.days
 
 import nl.sbmf21.aoc.common.ADay
+import nl.sbmf21.aoc.common.Vector2
 
 class Day20(input: List<String>) : ADay(input) {
 
-    private val algorithm = input[0].map { if (it == '#') 1 else 0 }
-    private val image = input.subList(2, input.size).map { r -> r.map { if (it == '#') 1 else 0 } }
+    private val algo = input[0].map { if (it == '#') 1 else 0 }
+    private val img = input.subList(2, input.size).map { r -> r.map { if (it == '#') 1 else 0 } }
+    private val neigh = (-1..1).map { y -> (-1..1).map { x -> Vector2(x, y) } }.flatten()
 
     override fun part1() = run(2)
     override fun part2() = run(50)
 
-    private fun run(times: Int) = (1..times).fold(image) { image, i ->
-        List(image.size + 2) { y -> MutableList(image.size + 2) { x -> check(y - 1, x - 1, image, i) } }
-    }.sumOf { r -> r.count { it == 1 } }
+    private fun run(times: Int) = (1..times)
+        .fold(img) { img, i -> List(img.size + 2) { y -> MutableList(img.size + 2) { x -> check(y, x, img, i) } } }
+        .sumOf { r -> r.count { it == 1 } }
 
-    private fun check(y: Int, x: Int, image: List<List<Int>>, pass: Int) = algorithm[(y - 1..y + 1).map { cy ->
-        (x - 1..x + 1).map { cx ->
-            if (cy in image.indices && cx in image[cy].indices) image[cy][cx]
-            else algorithm[if (algorithm[0] == 1) pass % 2 * algorithm.lastIndex else 0]
-        }
-    }.flatten().joinToString("").toInt(2)]
+    private fun check(y: Int, x: Int, img: List<List<Int>>, pass: Int) = algo[neigh.map { Vector2(x - 1, y - 1) + it }
+        .map { if (it.y in img.indices && it.x in img[it.y].indices) img[it.y][it.x] else algo[if (algo[0] == 1) pass % 2 * algo.lastIndex else 0] }
+        .joinToString("").toInt(2)]
 }
