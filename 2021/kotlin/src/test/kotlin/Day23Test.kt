b9a6eccba519b98cb730db2cc1d9a5c790e6c661
Boukje <boukje@switchware.io>
2021-12-23 06:40:46 +0100

    ¯\_(ツ)_/¯

---
+++ 2021/kotlin/src/test/kotlin/Day23Test.kt
@@ -0,0 +1,11 @@
+import nl.sbmf21.aoc21.days.Day23
+import org.junit.jupiter.api.Test
+
+class Day23Test {
+
+    @Test
+    fun testInput() = testDay(Day23::class.java, -1, -1)
+
+    @Test
+    fun testExample() = testDay(Day23::class.java, -1 /* 12_521 */, -1, true)
+}
