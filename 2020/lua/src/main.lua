a3571d062416451762012714f2271423501f9024
Boukje Maria Steur <boukje@switchware.io>
2022-04-14 12:36:58 +0200

    Clean up lua

--- 2020/lua/src/main.lua
+++ 2020/lua/src/main.lua
@@ -1,14 +1,16 @@
-function runDay(day)
+require('data')
+
+local function runDay(day)
     day = tostring(day)
 
-    require('day' .. day .. '.functions')
+    require(('day%d.functions'):format(day))
 
-    print('Day ' .. day)
-    print('Part 1: ' .. part1())
-    print('Part 2: ' .. part2())
+    print(('Day %d'):format(day))
+    print(('Part 1: %d'):format(part1()))
+    print(('Part 2: %d'):format(part2()))
     print()
 end
 
-for day = 1, 6 do
-    runDay(day)
+for day = 1, #days do
+    runDay(days[day].nr)
 end
