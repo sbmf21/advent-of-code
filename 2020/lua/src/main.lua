a5f511346d4a4227b315085cb9905c2666fc11ca
Boukje Maria Steur <boukje@switchware.io>
2022-04-14 15:48:08 +0200

    Add fancy reporting too for lua

--- 2020/lua/src/main.lua
+++ 2020/lua/src/main.lua
@@ -1,16 +1,37 @@
 require('data')
 
+local function run(fun)
+
+    local start = os.clock()
+    local value = fun()
+    local time = os.clock() - start
+
+    return value, time
+end
+
 local function runDay(day)
-    day = tostring(day)
-
+    local start = os.clock()
     require(('day%d.functions'):format(day))
+    local time = os.clock() - start
 
-    print(('Day %d'):format(day))
-    print(('Part 1: %d'):format(part1()))
-    print(('Part 2: %d'):format(part2()))
-    print()
+    local value1, time1 = run(part1)
+    local value2, time2 = run(part2)
+
+    return value1, value2, time, time1, time2
 end
 
-for day = 1, #days do
-    runDay(days[day].nr)
+for nr = 1, #days do
+    local day = days[nr]
+    local value1, value2, time, time1, time2 = runDay(day.nr)
+
+    day.value1 = value1
+    day.value2 = value2
+    day.timeTotal = time + time1 + time2
+    day.timeSetup = time
+    day.time1 = time1
+    day.time2 = time2
 end
+
+require('util.report')
+
+report(days)
