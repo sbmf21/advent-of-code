df579f8d9f6c4ff980820e2c572eeb4db6798139
Boukje Maria Steur <boukje@switchware.io>
2022-04-14 12:36:58 +0200

    Clean up lua

---
+++ 2020/lua/src/data.lua
@@ -0,0 +1,8 @@
+days = {
+    { nr = 1, part1 = 1019904, part2 = 176647680 },
+    { nr = 2, part1 = 515, part2 = 711 },
+    { nr = 3, part1 = 191, part2 = 1478615040 },
+    { nr = 4, part1 = 247, part2 = 145 },
+    { nr = 5, part1 = 922, part2 = 747 },
+    { nr = 6, part1 = 6903, part2 = 3493 },
+};
