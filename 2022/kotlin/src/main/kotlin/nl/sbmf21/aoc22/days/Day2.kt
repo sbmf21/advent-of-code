0f7aaa5de495793f9f29b106924d2dac86e08886
Boukje Maria Steur <boukje.steur@gmail.com>
2022-12-02 10:42:09 +0100

    []=[y=2022,d=2]

---
+++ 2022/kotlin/src/main/kotlin/nl/sbmf21/aoc22/days/Day2.kt
@@ -0,0 +1,82 @@
+package nl.sbmf21.aoc22.days
+
+import nl.sbmf21.aoc.common.ADay
+import nl.sbmf21.aoc22.days.Action.*
+import nl.sbmf21.aoc22.days.RockPaperScissor.*
+
+private enum class RockPaperScissor(val score: Int) {
+    ROCK(1),
+    PAPER(2),
+    SCISSOR(3);
+
+    fun beats(other: RockPaperScissor): Boolean {
+        return other == when (this) {
+            ROCK -> SCISSOR
+            PAPER -> ROCK
+            SCISSOR -> PAPER
+        }
+    }
+
+    companion object {
+        fun from(input: String) = when (input) {
+            "A", "X" -> ROCK
+            "B", "Y" -> PAPER
+            "C", "Z" -> SCISSOR
+            else -> throw Error()
+        }
+    }
+}
+
+private enum class Action(val score: Int) {
+    LOSE(0),
+    DRAW(3),
+    WIN(6);
+
+    companion object {
+        fun from(input: String) = when (input) {
+            "X" -> LOSE
+            "Y" -> DRAW
+            "Z" -> WIN
+            else -> throw Error()
+        }
+    }
+}
+
+class Day2(input: List<String>) : ADay(input) {
+
+    override fun part1() = input.sumOf {
+        val round = it.split(" ", limit = 2).run {
+            Pair(RockPaperScissor.from(this[0]), RockPaperScissor.from(this[1]))
+        }
+
+        val action =
+            if (round.second.beats(round.first)) WIN
+            else if (round.first == round.second) DRAW
+            else LOSE
+
+        round.second.score + action.score
+    }
+
+    override fun part2() = input.sumOf {
+        val round = it.split(" ", limit = 2).run {
+            Pair(RockPaperScissor.from(this[0]), Action.from(this[1]))
+        }
+
+        val choice = when (round.second) {
+            LOSE -> when (round.first) {
+                ROCK -> SCISSOR
+                PAPER -> ROCK
+                SCISSOR -> PAPER
+            }
+
+            DRAW -> round.first
+            WIN -> when (round.first) {
+                ROCK -> PAPER
+                PAPER -> SCISSOR
+                SCISSOR -> ROCK
+            }
+        }
+
+        round.second.score + choice.score
+    }
+}
---
+++ 2022/kotlin/src/test/kotlin/Day2Test.kt
@@ -0,0 +1,11 @@
+import nl.sbmf21.aoc22.days.Day2
+import org.junit.jupiter.api.Test
+
+class Day2Test {
+
+    @Test
+    fun testInput() = testDay(Day2::class.java, 15632, 14416)
+
+    @Test
+    fun testExample() = testDay(Day2::class.java, 15, 12, true)
+}
