23235775701d87943f05478d64c92be06c8671c9
Boukje Maria Steur <boukje.steur@gmail.com>
2022-12-03 17:23:49 +0100

    []=[y=2022,d=3]

---
+++ 2022/kotlin/src/main/kotlin/nl/sbmf21/aoc22/days/Day3.kt
@@ -0,0 +1,20 @@
+package nl.sbmf21.aoc22.days
+
+import nl.sbmf21.aoc.common.ADay
+
+class Day3(input: List<String>) : ADay(input) {
+
+    override fun part1() = input.sumOf { items ->
+        prio(items.substring(0, items.length / 2).first { items.substring(items.length / 2).contains(it) })
+    }
+
+    override fun part2() = input.chunked(3).sumOf { group ->
+        prio(group[0].first { group[1].contains(it) && group[2].contains(it) })
+    }
+
+    private fun prio(char: Char): Int {
+        val prio = char.code and 31
+        return if (char.code and 32 == 0) prio + 26
+        else prio
+    }
+}
