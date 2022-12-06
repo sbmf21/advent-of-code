d697e0f37b869d747432177d6abd67f66f52536a
Boukje Maria Steur <boukje.steur@gmail.com>
2022-12-06 20:57:01 +0100

    []=[y=2021,d=22,p=2]

--- 2021/kotlin/src/main/kotlin/nl/sbmf21/aoc21/days/util.kt
+++ 2021/kotlin/src/main/kotlin/nl/sbmf21/aoc21/days/util.kt
@@ -1,3 +1,4 @@
 package nl.sbmf21.aoc21.days
 
 internal fun MatchResult.toInt(name: String) = this.groups[name]!!.value.toInt()
+internal fun MatchResult.toLong(name: String) = this.groups[name]!!.value.toLong()
