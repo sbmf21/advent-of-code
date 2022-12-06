063784671c636f2d99514a8a3915485652d7ba36
Boukje Maria Steur <boukje.steur@gmail.com>
2022-12-06 06:20:27 +0100

    []=[y=2022,d=6]

---
+++ 2022/kotlin/src/main/kotlin/nl/sbmf21/aoc22/days/Day6.kt
@@ -0,0 +1,21 @@
+package nl.sbmf21.aoc22.days
+
+import nl.sbmf21.aoc.common.ADay
+
+class Day6(input: List<String>) : ADay(input) {
+
+    private val dataStream = input[0]
+
+    override fun part1() = findMarker(4)
+
+    override fun part2() = findMarker(14)
+
+    private fun findMarker(length: Int): Int {
+        for (i in 0..dataStream.length - length) {
+            if (dataStream.substring(i until i + length).toList().distinct().size == length) {
+                return i + length
+            }
+        }
+        return -1
+    }
+}
---
+++ 2022/kotlin/src/test/kotlin/Day6Test.kt
@@ -0,0 +1,22 @@
+import nl.sbmf21.aoc22.days.Day6
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class Day6Test {
+
+    @Test
+    fun testInput() = testDay(Day6::class.java, 1802, 3551)
+
+    @Test
+    fun testExample() = mapOf(
+        "mjqjpqmgbljsphdztnvjfqwrcgsmlb" to (7 to 19),
+        "bvwbjplbgvbhsrlpgdmjqwftvncz" to (5 to 23),
+        "nppdvjthqldpwncqszvftbrmjlhg" to (6 to 23),
+        "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg" to (10 to 29),
+        "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw" to (11 to 26),
+    ).forEach { (data, end) ->
+        val day = buildWithInput(Day6::class.java, listOf(data))
+        assertEquals(end.first, day.part1())
+        assertEquals(end.second, day.part2())
+    }
+}
