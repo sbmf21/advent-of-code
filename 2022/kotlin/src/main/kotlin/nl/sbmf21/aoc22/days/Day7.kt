1cdda1a12d907b17d8cdf461413aec02339ab6d3
Boukje Maria Steur <boukje.steur@gmail.com>
2022-12-07 06:47:21 +0100

    []=[y=2022,d=7]

---
+++ 2022/kotlin/src/main/kotlin/nl/sbmf21/aoc22/days/Day7.kt
@@ -0,0 +1,78 @@
+package nl.sbmf21.aoc22.days
+
+import nl.sbmf21.aoc.common.ADay
+
+class Day7(input: List<String>) : ADay(input) {
+
+    private val root: Dir = mapFileStructure()
+    private val directories: List<Dir> = getDirectories(root).sortedBy { it.size }
+    private val freeSpace: Int = 70_000_000 - root.size
+
+    override fun part1() = getSizesBelow100k(root)
+
+    override fun part2() = directories.first { freeSpace + it.size > 30_000_000 }.size
+
+    private fun mapFileStructure(): Dir {
+        lateinit var root: Dir
+        var current: Dir? = null
+
+        input.fold(mutableListOf<Command>()) { commands, it ->
+            if (it[0] == '$') commands.add(Command(it.substring(2)))
+            else commands.last().output += it
+            commands
+        }.forEach { cmd ->
+            when (cmd.command) {
+                "cd" -> current =
+                    if (current == null) Dir(cmd.arg!!).also { root = it }
+                    else if (cmd.arg == "..") current!!.parent
+                    else current!!.files.first { it is Dir && it.name == cmd.arg } as Dir
+
+                "ls" -> cmd.output.forEach {
+                    val parts = it.split(" ", limit = 2)
+                    current!!.files +=
+                        if (parts[0] == "dir") Dir(parts[1], current)
+                        else File(parts[1], parts[0].toInt(), current!!)
+                }
+            }
+        }
+
+        return root
+    }
+
+    private fun getDirectories(dir: Dir): List<Dir> {
+        val list = mutableListOf(dir)
+        for (f in dir.files.filterIsInstance<Dir>()) list += getDirectories(f)
+        return list
+    }
+
+    private fun getSizesBelow100k(dir: Dir): Int {
+        var size = if (dir.size <= 100_000) dir.size else 0
+        for (file in dir.files.filterIsInstance<Dir>()) size += getSizesBelow100k(file)
+        return size
+    }
+}
+
+private class Command(input: String) {
+    val command: String
+    val arg: String?
+    val output = mutableListOf<String>()
+
+    init {
+        val data = input.split(" ", limit = 2)
+        command = data[0]
+        arg = if (data.size > 1) data[1] else null
+    }
+}
+
+private interface FileData {
+    val size: Int
+}
+
+@Suppress("unused") // name and parent are unused, but I like 'em
+private class File(val name: String, override val size: Int, val parent: Dir) : FileData
+
+private class Dir(val name: String, val parent: Dir? = null) : FileData {
+    val files = mutableListOf<FileData>()
+
+    override val size: Int get() = files.sumOf { it.size }
+}
---
+++ 2022/kotlin/src/test/kotlin/Day7Test.kt
@@ -0,0 +1,11 @@
+import nl.sbmf21.aoc22.days.Day7
+import org.junit.jupiter.api.Test
+
+class Day7Test {
+
+    @Test
+    fun testInput() = testDay(Day7::class.java, 1_583_951, 214_171)
+
+    @Test
+    fun testExample() = testDay(Day7::class.java, 95_437, 24_933_642, true)
+}
