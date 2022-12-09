e7d1de172a5c1193aae953ba03bd02043a2d9a75
Boukje Maria Steur <boukje.steur@gmail.com>
2022-12-09 20:11:44 +0100

    Add simulation for 2022 day 5

--- 2022/kotlin/src/main/kotlin/nl/sbmf21/aoc22/Aoc.kt
+++ 2022/kotlin/src/main/kotlin/nl/sbmf21/aoc22/Aoc.kt
@@ -1,15 +1,24 @@
 package nl.sbmf21.aoc22
 
+import nl.sbmf21.aoc.common.ADay
 import nl.sbmf21.aoc.common.AocBase
+import nl.sbmf21.aoc22.days.Day5
 import nl.sbmf21.aoc22.days.Day9
+import nl.sbmf21.aoc22.days.simulations.Crates
+import nl.sbmf21.aoc22.days.simulations.Simulation
 import nl.sbmf21.aoc22.days.simulations.Snake
 
 class Aoc : AocBase("2022")
 
 fun main(args: Array<String>) {
     val aoc = Aoc()
-    if ("--simulate" in args) {
-        println("Simulating day 9")
-        Snake(aoc.days[8].build() as Day9).runSnake()
-    } else aoc.exec(args)
+    if ("--crates" in args) simulate<Day5, Crates>(aoc) { Crates(it) }
+    else if ("--snake" in args) simulate<Day9, Snake>(aoc) { Snake(it) }
+    else aoc.exec(args)
+}
+
+private inline fun <reified T : ADay, reified S : Simulation<T>> simulate(aoc: Aoc, init: (T) -> S) {
+    val day = aoc.days.first { it.clazz == T::class.java }.build() as T
+    println("Simulating day ${day.number}")
+    init.invoke(day).simulate()
 }
