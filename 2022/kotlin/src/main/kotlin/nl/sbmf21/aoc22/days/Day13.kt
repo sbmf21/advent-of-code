78333d7a82f594785da22d6bc76aea5ff905ca90
Boukje Maria Steur <boukje.steur@gmail.com>
2022-12-14 07:17:14 +0100

    Cleanup day 13

--- 2022/kotlin/src/main/kotlin/nl/sbmf21/aoc22/days/Day13.kt
+++ 2022/kotlin/src/main/kotlin/nl/sbmf21/aoc22/days/Day13.kt
@@ -6,92 +6,71 @@ import kotlin.math.min
 
 class Day13(input: List<String>) : ADay(input) {
 
-    private val packets = input.chunked(3).map { Pair(read(it[0]), read(it[1])) }
+    private val comparator = PacketCompare()
+    private val packets = input.chunked(3).map { Pair(Packet.from(it[0]), Packet.from(it[1])) }
+    private val dividers = listOf("[[2]]", "[[6]]").map { Packet.from(it, true) }
 
     override fun part1() = packets
-        .asSequence()
-        .mapIndexed { index, it -> if (compare(it.first.input, it.second.input) == -1) index + 1 else 0 }
+        .mapIndexed { index, it -> if (comparator.compare(it.first, it.second) == -1) index + 1 else 0 }
         .sum()
 
     override fun part2() = packets
-        .asSequence()
-        .map { listOf(it.first, it.second) }
-        .flatten()
-        .toMutableList()
-        .also { it.addAll(listOf(read("[[2]]", true), read("[[6]]", true))) }
-        .sortedWith(PacketCompare())
+        .flatMap { listOf(it.first, it.second) }
+        .plus(dividers)
+        .sortedWith(comparator)
         .mapIndexedNotNull { index, packet -> if (packet.divider) index + 1 else null }
         .prod()
 }
 
-private class PacketCompare : Comparator<Packet> {
-    override fun compare(left: Packet, right: Packet) = compare(left.input, right.input)
-}
+private class Packet(val parent: Packet? = null, val divider: Boolean = false) : MutableList<Any> by mutableListOf() {
+    companion object {
+        fun from(line: String, divider: Boolean = false): Packet {
+            val root = Packet(divider = divider)
+            var current: Packet = root
+            var sameList = false
 
-private fun compare(left: List<*>, right: List<*>): Int {
+            var i = 1
+            while (i < line.length) {
+                var c = line[i++]
 
-    for (i in 0 until min(left.size, right.size)) {
-        var cLeft = left[i]
-        var cRight = right[i]
+                var number = ""
+                while (c in '0'..'9') {
+                    number += c
+                    c = line[i++]
+                }
+                if (number.isNotEmpty()) current.add(number.toInt())
 
-        if (cLeft is Packet) cLeft = cLeft.input
-        if (cRight is Packet) cRight = cRight.input
+                when (c) {
+                    '[' -> Packet(parent = if (sameList) current.parent else current).also {
+                        sameList = false
+                        current.add(it)
+                        current = it
+                    }
 
-        if (cLeft is Int && cRight is Int)
-            return if (cLeft < cRight) -1
-            else if (cLeft > cRight) 1
-            else continue
-        if (cRight is Int) cRight = listOf(cRight)
-        else if (cLeft is Int) cLeft = listOf(cLeft)
-
-        val compare = compare(cLeft as List<*>, cRight as List<*>)
-        if (compare != 0) return compare
-    }
-
-    return left.size.compareTo(right.size)
-}
-
-private fun read(line: String, divider: Boolean = false): Packet {
-
-    var i = 1
-
-    val root = Packet(divider = divider)
-    var current: Packet = root
-
-    var sameList = false
-
-    while (i < line.length) {
-        var c = line[i]
-
-        var number = ""
-        while (c in '0'..'9') {
-            number += c
-            c = line[++i]
-        }
-
-        if (number.isNotEmpty()) current.input.add(number.toInt())
-
-        when (c) {
-            '[' -> {
-                val packet = Packet(parent = if (sameList) current.parent else current)
-                sameList = false
-                current.input.add(packet)
-                current = packet
+                    ']' -> current.parent?.also { current = it }
+                    ',' -> sameList = true
+                }
             }
-
-            ']' -> {
-                val parent = current.parent
-                if (parent != null) current = parent
-            }
-
-            ',' -> sameList = true
+            return root
         }
-
-        i++
     }
-    return root
 }
 
-private class Packet(val parent: Packet? = null, val divider: Boolean = false) {
-    val input = mutableListOf<Any>()
+private class PacketCompare : Comparator<List<*>> {
+    override fun compare(left: List<*>, right: List<*>): Int {
+        for (i in 0 until min(left.size, right.size)) {
+            var cLeft = left[i]
+            var cRight = right[i]
+
+            if (cLeft is Int && cRight is Int)
+                return if (cLeft < cRight) -1
+                else if (cLeft > cRight) 1
+                else continue
+            else if (cLeft is Int) cLeft = listOf(cLeft)
+            else if (cRight is Int) cRight = listOf(cRight)
+
+            compare(cLeft as List<*>, cRight as List<*>).also { if (it != 0) return it }
+        }
+        return left.size.compareTo(right.size)
+    }
 }
