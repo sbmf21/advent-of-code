5304192d99d27a2fe3b896b76dc38f14e9c22363
Boukje Maria Steur <boukje.steur@gmail.com>
2022-12-14 07:18:04 +0100

    []=[y=2022,d=14]

---
+++ 2022/kotlin/src/test/kotlin/Day14Test.kt
@@ -0,0 +1,11 @@
+import nl.sbmf21.aoc22.days.Day14
+import org.junit.jupiter.api.Test
+
+class Day14Test {
+
+    @Test
+    fun testInput() = testDay(Day14::class.java, 1406, 20870)
+
+    @Test
+    fun testExample() = testDay(Day14::class.java, 24, 93, true)
+}
