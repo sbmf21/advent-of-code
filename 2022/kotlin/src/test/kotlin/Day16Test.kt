1b7829fafdc45171858d3495ed5dac2ca1fca444
Boukje Maria Steur <boukje.steur@gmail.com>
2022-12-16 07:36:27 +0100

    []=[y=2022,d=16,p=???]

---
+++ 2022/kotlin/src/test/kotlin/Day16Test.kt
@@ -0,0 +1,15 @@
+import nl.sbmf21.aoc22.days.Day16
+import org.junit.jupiter.api.Test
+
+class Day16Test {
+
+    @Test
+    fun testInput() {
+        testDay(Day16::class.java, -1, -1)
+    }
+
+    @Test
+    fun testExample() {
+        testDay(Day16::class.java, 1651, -1, true)
+    }
+}
