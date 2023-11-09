639400143999c1a233f4528e3d284c32459b6490
Boukje Maria Steur <boukje.steur@gmail.com>
2023-11-09 12:01:57 +0100

    add 2023 day 1 start

---
+++ 2023/kotlin/src/main/kotlin/nl/sbmf21/aoc23/days/Day1.kt
@@ -0,0 +1,10 @@
+package nl.sbmf21.aoc23.days
+
+import nl.sbmf21.aoc.common.ADay
+
+class Day1(input: List<String>) : ADay(input) {
+
+    override fun part1() = -1
+
+    override fun part2() = -1
+}
