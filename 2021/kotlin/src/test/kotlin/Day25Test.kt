c07a2ac09fa0826d41164890dfffbf2a8b7a623b
Boukje Maria Steur <boukje.steur@gmail.com>
2023-11-12 16:46:14 +0100

    Cleanup day 25

--- 2021/kotlin/src/test/kotlin/Day25Test.kt
+++ 2021/kotlin/src/test/kotlin/Day25Test.kt
@@ -4,8 +4,8 @@ import org.junit.jupiter.api.Test
 class Day25Test {
 
     @Test
-    fun testInput() = testDay(Day25::class.java, 456, -1)
+    fun testInput() = testDay(Day25::class.java, 456, 50)
 
     @Test
-    fun testExample() = testDay(Day25::class.java, 58, -1, true)
+    fun testExample() = testDay(Day25::class.java, 58, 50, true)
 }
