d3b4a3a74fbd5bc7b2d70178afb01d8041fa5410
Boukje Maria Steur <boukje.steur@gmail.com>
2023-11-13 21:26:28 +0100

    []=[y=2022,d=25]

---
+++ 2022/kotlin/src/main/kotlin/nl/sbmf21/aoc22/days/Day25.kt
@@ -0,0 +1,27 @@
+package nl.sbmf21.aoc22.days
+
+import nl.sbmf21.aoc.common.ADay
+
+class Day25(input: List<String>) : ADay(input) {
+
+    override fun part1() = encode(input.map(::decode).sum())
+    override fun part2() = 50
+
+    companion object {
+        private const val DIGITS = "=-012"
+        private const val BASE = 5
+        private const val OFFSET = 2
+
+        fun decode(snafu: String) = snafu.fold(0L) { n, c -> n * BASE + DIGITS.indexOf(c) - OFFSET }
+
+        fun encode(decimal: Long) = buildString {
+            var n = decimal
+            while (n > 0) {
+                val index = (n + OFFSET) % BASE
+                append(DIGITS[index.toInt()])
+                if (index < OFFSET) n += BASE
+                n /= BASE
+            }
+        }.reversed()
+    }
+}
