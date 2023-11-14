fef37f733e982c029b1bf108a7e7ba942380b0e7
Boukje Maria Steur <boukje.steur@gmail.com>
2023-11-14 17:55:57 +0100

    []=[y=2015,d=12]

--- README.md
+++ README.md
@@ -157,7 +157,7 @@ are done.
 <td style="text-align: center">✅</td>
 <td style="text-align: center">✅</td>
 <td style="text-align: center"></td>
-<td style="text-align: center"></td>
+<td style="text-align: center">✅</td>
 </tr>
 
 <tr>
--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,7 @@ group = "nl.sbmf21.aoc"
 
 val annotationsVersion by extra { "24.0.1" }
 val asciiTableVersion by extra { "0.3.2" }
+val gsonVersion by extra { "2.10.1" }
 val junitJupiterVersion by extra { "5.10.1" }
 val reflectionsVersion by extra { "0.10.2" }
 val sbmfMathVersion by extra { "1.4.2" }
