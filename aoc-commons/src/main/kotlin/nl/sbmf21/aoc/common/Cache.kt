c826dec3f9494432fc072800d62a00d565ed69ca
Boukje Maria Steur <boukje.steur@gmail.com>
2023-11-14 18:07:18 +0100

    Fix cache multithreading

--- aoc-commons/src/main/kotlin/nl/sbmf21/aoc/common/Cache.kt
+++ aoc-commons/src/main/kotlin/nl/sbmf21/aoc/common/Cache.kt
@@ -1,17 +1,15 @@
 package nl.sbmf21.aoc.common
 
+import java.util.Collections.synchronizedMap
+
 private typealias DayCache = Pair<Int, List<String>>
 
 internal object Cache {
 
-    private val backing = mutableMapOf<Class<out Day>, DayCache>()
+    private val backing = synchronizedMap(mutableMapOf<Class<out Day>, DayCache>())
 
     operator fun set(clazz: Class<out Day>, data: DayCache) {
         backing[clazz] = data
-    }
-
-    infix fun unset(clazz: Class<out Day>) {
-        backing.remove(clazz)
     }
 
     operator fun get(clazz: Class<out Day>): DayCache {
