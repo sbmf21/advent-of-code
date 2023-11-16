c0fcdfe5e97e7f16b3f305ccb6ee0a2f0be3ad20
Boukje Maria Steur <boukje.steur@gmail.com>
2023-11-16 15:51:29 +0100

    Add colors and improve code for table generating

--- aoc-commons/src/main/kotlin/nl/sbmf21/aoc/common/Color.kt
+++ aoc-commons/src/main/kotlin/nl/sbmf21/aoc/common/Color.kt
@@ -4,8 +4,11 @@ package nl.sbmf21.aoc.common
  * ASCII colors https://www.lihaoyi.com/post/BuildyourownCommandLinewithANSIescapecodes.html
  */
 enum class Color(private val code: String) {
+    RED("\u001b[31m"),
     GREEN("\u001b[32m"),
     YELLOW("\u001b[33m"),
+    BLUE("\u001b[34m"),
+
     LIGHT_GREEN("\u001b[38;5;46m"),
     GRAY("\u001B[38;5;245m"),
     WHITE("\u001b[38;5;255m"),
@@ -14,9 +17,15 @@ enum class Color(private val code: String) {
     BACK_DARK_GREEN("\u001b[48;5;22m"),
     BACK_BLACK("\u001b[48;5;0m"),
     BACK_WHITE("\u001b[48;5;15m"),
+
+    BOLD("\u001b[1m"),
     RESET("\u001b[0m");
 
     operator fun plus(string: String) = code + string
 
     override fun toString() = code
+
+    companion object {
+        val REGEX = Regex("(\\x9B|\\x1B\\[)[0-?]*[ -/]*[@-~]")
+    }
 }
--- aoc-commons/src/main/kotlin/nl/sbmf21/aoc/common/Format.kt
+++ aoc-commons/src/main/kotlin/nl/sbmf21/aoc/common/Format.kt
@@ -1,6 +1,6 @@
 package nl.sbmf21.aoc.common
 
-import kotlin.math.round
+import nl.sbmf21.aoc.common.Color.*
 
 internal object Format {
     private const val SECONDS = 1_000_000_000.0
@@ -11,11 +11,29 @@ internal object Format {
     private const val MILLIS_SUFFIX = "ms"
     private const val MICROS_SUFFIX = "μs"
 
-    fun timeString(time: Long, trimSuffix: Boolean = false) = when {
-        time > SECONDS -> format(time, SECONDS, SECONDS_SUFFIX, trimSuffix)
-        time > MILLIS -> format(time, MILLIS, MILLIS_SUFFIX, trimSuffix)
-        time > MICROS -> format(time, MICROS, MICROS_SUFFIX, trimSuffix)
-        else -> "$time ns"
+    fun timeString(time: Long, trimSuffix: Boolean = false, color: Color = RESET) = when {
+        time >= SECONDS -> {
+            val seconds = time / SECONDS
+            val actualColor = when {
+                seconds > 5 -> RED
+                else -> YELLOW
+            }
+
+            actualColor + format(time, SECONDS, SECONDS_SUFFIX, trimSuffix)
+        }
+
+        time >= MILLIS -> {
+            val seconds = time / MILLIS
+            val actualColor = when {
+                seconds > 500 -> YELLOW
+                else -> color
+            }
+
+            actualColor + format(time, MILLIS, MILLIS_SUFFIX, trimSuffix)
+        }
+
+        time >= MICROS -> color + format(time, MICROS, MICROS_SUFFIX, trimSuffix)
+        else -> "$color$time ns"
     }
 
     fun stringifyNumber(n: Any) = when (n) {
@@ -25,5 +43,5 @@ internal object Format {
     }
 
     private fun format(time: Long, value: Double, suffix: String, trimSuffix: Boolean) =
-        "${round((time / value) * 100) / 100} ${if (trimSuffix) suffix.trim() else suffix}"
+        "%,.2f".format(time / value) + "$RESET ${if (trimSuffix) suffix.trim() else suffix}"
 }
