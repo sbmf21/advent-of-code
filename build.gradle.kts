5680e9e98e875c5b7d35236ec9a44ced583e306f
Boukje Maria Steur <boukje.steur@gmail.com>
2023-12-08 08:02:27 +0100

    []=[y=2023,d=8]

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ val annotationsVersion by extra { "24.0.1" }
 val gsonVersion by extra { "2.10.1" }
 val junitJupiterVersion by extra { "5.10.1" }
 val reflectionsVersion by extra { "0.10.2" }
-val sbmfMathVersion by extra { "1.4.2" }
+val sbmfMathVersion by extra { "1.5.0" }
 val slf4jVersion by extra { "2.0.9" }
 
 repositories {
