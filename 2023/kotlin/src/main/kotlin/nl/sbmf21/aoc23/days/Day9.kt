6ad8d4e5fcc925eb6329712ced0bd4d9d36437e2
Boukje Maria Steur <boukje.steur@gmail.com>
2023-12-09 07:09:07 +0100

    cleanup 2

--- 2023/kotlin/src/main/kotlin/nl/sbmf21/aoc23/days/Day9.kt
+++ 2023/kotlin/src/main/kotlin/nl/sbmf21/aoc23/days/Day9.kt
@@ -7,11 +7,10 @@ class Day9 : Day() {
 
     private val oasisHistory = input.map { it.split(" ").mapToLongs() }.map { start ->
         buildList history@{
-            this += start.first() to start.last()
             var current = start
             while (!current.all { it == 0L }) current = buildList {
+                this@history += current.first() to current.last()
                 for (idx in 0..<current.lastIndex) this += current[idx + 1] - current[idx]
-                this@history += first() to last()
             }
             reverse()
         }
@@ -20,7 +19,5 @@ class Day9 : Day() {
     override fun part1() = solve { num, list -> num + list.second }
     override fun part2() = solve { num, list -> list.first - num }
 
-    private fun solve(extrapolate: (Long, Pair<Long, Long>) -> Long) = oasisHistory.sumOf { history ->
-        (1..history.lastIndex).fold(0L) { num, i -> extrapolate(num, history[i]) }
-    }
+    private fun solve(extrapolate: (Long, Pair<Long, Long>) -> Long) = oasisHistory.sumOf { it.fold(0L, extrapolate) }
 }
