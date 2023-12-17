18fb9f623e8506c826d9aa9feb076f6f8c199169
Boukje Maria Steur <boukje.steur@gmail.com>
2023-12-17 10:58:18 +0100

    []=[y=2023,d=13,p=2]

--- 2023/kotlin/src/main/kotlin/nl/sbmf21/aoc23/days/Day13.kt
+++ 2023/kotlin/src/main/kotlin/nl/sbmf21/aoc23/days/Day13.kt
@@ -2,83 +2,32 @@ package nl.sbmf21.aoc23.days
 
 import nl.sbmf21.aoc.common.Day
 import nl.sbmf21.aoc.common.split
-import nl.sbmf21.aoc23.TODO
+import nl.sbmf21.aoc.common.transpose
 import nl.sbmf21.math.Vector2i
 import nl.sbmf21.math.by
-import kotlin.math.max
-import kotlin.math.min
 
 class Day13 : Day() {
 
-    private val patterns = input.split(true, String::isBlank).map { it to summarize(it)!! }
+    override fun part1() = solve(0)
+    override fun part2() = solve(1)
 
-    override fun part1() = patterns.sumOf { (_, pos) -> summary(pos) }
-
-    override fun part2(): String {
-        return TODO
-
-//        return patterns
-//            .map { (map, original) ->
-//                for (y in map.indices) {
-//                    for (x in map[y].indices) {
-//                        val testVal = summarize(flip(map, x, y), true)
-//                        if (testVal != null && testVal != original) return@map testVal
-//                    }
-//                }
-//
-//                println("no change found")
-//
-//                original
-//            }
-//            .sumOf(::summary)
+    private fun solve(differ: Int) = input.split(true, String::isEmpty).sumOf {
+        summarize(findFold(it, differ) by findFold(it.transpose(), differ))
     }
 
-    private fun summary(pos: Vector2i) = pos.x + 100 * pos.y
-
-    private fun summarize(it: List<String>): Vector2i? {
-
-        y@ for (i in 1..it.lastIndex) {
-            if (it[i - 1] == it[i]) {
-                val remaining = max(0, min(it.lastIndex - i, i - 1))
-
-                for (ci in 0..remaining) {
-                    if (it[i - 1 - ci] != it[i + ci]) continue@y
-                }
-
-                return 0 by i
+    private fun findFold(pattern: List<String>, differ: Int): Int {
+        for (scanLine in 0 until pattern[0].lastIndex) {
+            var difference = 0
+            for (current in 0..scanLine) {
+                val reflection = (scanLine - current) + scanLine + 1
+                if (reflection !in pattern[0].indices) continue
+                difference += pattern.indices.count { pattern[it][current] != pattern[it][reflection] }
             }
+            if (difference == differ) return scanLine + 1
         }
 
-        val verticals = buildList {
-            for (i in it[0].indices) {
-                add(it.joinToString("") { l -> "${l[i]}" })
-            }
-        }
-
-        x@ for (i in 1..verticals.lastIndex) {
-            if (verticals[i - 1] == verticals[i]) {
-                val remaining = max(0, min(verticals.lastIndex - i, i - 1))
-
-                for (ci in 0..remaining) {
-                    if (verticals[i - 1 - ci] != verticals[i + ci]) continue@x
-                }
-
-                return i by 0
-            }
-        }
-
-        return null
+        return 0
     }
 
-//    private fun flip(map: List<String>, x: Int, y: Int) = map.mapIndexed { cy, line ->
-//        if (cy == y) {
-//            val next = when (line[x]) {
-//                '#' -> '.'
-//                '.' -> '#'
-//                else -> throw Error("cannot flip")
-//            }
-//
-//            line.substring(0..<x) + next + line.substring(x + 1..line.lastIndex)
-//        } else line
-//    }
+    private fun summarize(pos: Vector2i) = pos.x + 100 * pos.y
 }
