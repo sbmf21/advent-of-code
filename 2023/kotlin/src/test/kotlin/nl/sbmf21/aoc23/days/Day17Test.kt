c3615fcc60104e9b2e40abce5aa611258d3187e5
Boukje Maria Steur <boukje.steur@gmail.com>
2023-12-17 07:35:26 +0100

    []=[y=2023,d=17]

---
+++ 2023/kotlin/src/test/kotlin/nl/sbmf21/aoc23/days/Day17Test.kt
@@ -0,0 +1,13 @@
+package nl.sbmf21.aoc23.days
+
+import nl.sbmf21.aoc.testing.testDay
+import org.junit.jupiter.api.Test
+
+class Day17Test {
+
+    @Test
+    fun testInput() = testDay(Day17::class.java, 1246, 1389)
+
+    @Test
+    fun testExample() = testDay(Day17::class.java, 102, 94, true)
+}
